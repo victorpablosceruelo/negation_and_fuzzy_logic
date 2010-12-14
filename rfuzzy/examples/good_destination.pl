608b517a01af6df06566631a4674d38a72bafb0f
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2010-12-14 12:00:50 +0000

    good_destination example.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@91 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ rfuzzy/examples/good_destination.pl
@@ -0,0 +1,42 @@
+:- module(good_destination,_,[rfuzzy, clpr, debugger_pkg]).
+
+% Define the individuals belonging to the set cities.
+city(madrid).
+city(istambul).
+city(moscow).
+city(sydney).
+
+% Define that only cities are valid individuals for
+% the fuzzy set good_destination.
+:- set_prop good_destination/1 => city/1.
+
+% A city is a good destination with a truth value of 0.1
+% if we can not compute a more accurate value.
+:- default(good_destination/1, 0.3).
+
+% The rule to determine the grade of belonging of 
+% a player to the fuzzy set of good_player has a 
+% confidence of 0.8. Its result is a combination of 
+% how much (the truth value) he/she is swift, tall and 
+% an experienced player. 
+good_destination(Place) cred (prod,1) :~ prod((nice_weather(Place), many-sights(Place))).
+
+
+:- set_prop nice_weather/1 => city/1.
+:- default(nice_weather/1, 0.5).
+
+nice_weather(madrid, 0.8).
+nice_weather(madrid, 0.7).
+nice_weather(madrid, 0.2).
+
+:- set_prop many_sights/1 => city/1.
+:- default(many_sights/1, 0.2).
+
+many_sights(madrid, 0.6).
+many_sights(istambul, 0.7).
+many_sights(sydney, 0.6).
+
+% Queries (examples).
+
+% ?- good_player(X, Y). 
+% ?- good_player(X, rat(4,125)). 
