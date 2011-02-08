0a31086b94a67c79b9f30f1e2516b538b7cc31f0
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-02-08 19:25:28 +0000

    Example fixed thanks to jfran.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@104 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- interface-prolog/example1.pl
+++ interface-prolog/example1.pl
@@ -2,8 +2,8 @@
 
 :-use_module(library(write)).
 
-% This example does not work as expected, but we still do not know 
-% where id the problem.
+% This example works as expected,  
+% thanks to Jose F. Morales ( jfran )
 
 p(X) :- X .>. 0.
 
@@ -14,12 +14,17 @@ local_write_vars([Var|Vars]) :-
 	write_attribute(Var), display(' , '),
 	local_write_vars(Vars).
 
-main(Vars) :- query(Q, Vars), call(Q), 
-	display('display: '), display(Q), nl,
-	display('write_term: '), write_term(Q, [portrayed(true)]), nl, 
+local_write_answers(Vars, Dict) :- 
+%	display('display: '), display(Q), nl,
+%	display('write_term: '), write_term(Q, [portrayed(true)]), nl, 
 %	write_out(Q, options(_,_,_,true,_), _,_,_,_,_,_), nl,
-	display('write_attribute: [ '), local_write_vars(Vars), display(' ] '), nl. 
+%	display('write_attribute: [ '), local_write_vars(Vars), display(' ] '), nl. 
 %	prettyvars(Vars).
+	display('dump_constraints: '), dump_constraints(Vars, Dict, C), 
+	write(C), nl.
+
+main :- query(Q, Vars), call(Q),
+	local_write_answers(Vars, ['X']).
 
 :- multifile portray_attribute/2.
 portray_attribute(X) :- X = eqn_var(_,_,_,_,_), !, print(X).
