e121cc2e42c7858d694165457bc485593cf93bbd
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-02-08 13:20:32 +0000

    Problematic interface example.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@102 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ interface-prolog/example1.pl
@@ -0,0 +1,22 @@
+:- module(example1, _, [clpr]).
+
+:-use_module(library(write)).
+
+% This example does not work as expected, but we still do not know 
+% where id the problem.
+
+p(X) :- X .>. 0.
+
+query(p(X), [X]).
+
+local_write_vars([]).
+local_write_vars([Var|Vars]) :-
+	write_attribute(Var), display(' , '),
+	local_write_vars(Vars).
+
+main(Vars) :- query(Q, Vars), call(Q), 
+	display('display: '), display(Q), nl,
+	display('write_term: '), write_term(Q, [portrayed(true)]), nl, 
+%	write_out(Q, options(_,_,_,true,_), _,_,_,_,_,_), nl,
+	display('write_attribute: [ '), local_write_vars(Vars), display(' ] '), nl. 
+%	prettyvars(Vars).
