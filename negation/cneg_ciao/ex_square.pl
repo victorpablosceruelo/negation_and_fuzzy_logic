cdbad837fbbc90a52b4c640fd9238c4686c750b7
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-11-18 15:07:32 +0000

    A new example and some fixes.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@392 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ negation/cneg_ciao/ex_square.pl
@@ -0,0 +1,13 @@
+:- module(ex_square,_,[.(cneg), .(debugger_pkg)]).
+
+square(X) :- nat(Y), prod(Y, Y, X).
+
+nat(0).
+nat(s(X)) :- nat(X).
+
+prod(0, _Y, 0).
+prod(s(0), Y, Y).
+prod(s(s(X)), Y, Z) :- prod(s(X), Y, W), add(W, Y, Z).
+
+add(0, Y, Y).
+add(s(X), Y, s(Z)) :- add(X, Y, Z).
--- negation/cneg_ciao/ex_stuckey_fails.pl
+++ negation/cneg_ciao/ex_stuckey_fails.pl
@@ -1,4 +1,4 @@
-:- module(ex_stuckey_fails,_,[.(cneg), debugger_pkg]).
+:- module(ex_stuckey_fails,_,[.(cneg), .(debugger_pkg)]).
 
 
 p(X) :- disequality(X, Z, []), equality(Z, 3, []). 
