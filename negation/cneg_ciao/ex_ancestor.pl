0b281c30ae7b31a019c4b2b7eb8f814f2d9c4055
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-12-23 13:22:49 +0000

    Added new functionality.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@434 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- negation/cneg_ciao/ex_ancestor.pl
+++ negation/cneg_ciao/ex_ancestor.pl
@@ -1,14 +1,31 @@
-:- module(ex_ancestor,_,[.(cneg), debugger_pkg]).
+:- module(ex_ancestor_grandparent,_,[.(cneg), debugger_pkg]).
 % :- module(ex_ancestor,_,[.(cneg)]).
 
+test_parent(X, Y) :- cneg_rt([], parent(X, Y)), parent(X, Y).
+test_ancestor(X, Y) :- cneg_rt([], ancestor(X, Y)), ancestor(X, Y).
+test_grandparent(X, Y) :- cneg_rt([], parent(X, Y)), parent(X, Y).
 
-parent1(john, mary).
-parent1(john, peter).
-parent1(mary, joe).
-parent1(peter, susan).
+parent(marcus, daniel).
+
+parent2(bob,oscar).
+parent2(oscar,joan).
+
+parent(tom, john).
+parent(john, mary).
+parent(john, peter).
+parent(mary, joe).
+parent(peter, susan).
 
 ancestor(X, Y):-
- 	parent1(X, Y).
+ 	parent(X, Y).
 ancestor(X, Y):-
-	parent1(X, Z),
+	parent(X, Z),
 	ancestor(Z, Y).
+
+no_ancestor(X, Y) :- cneg_tr([], ancestor(X, Y)).
+
+grandparent(X,Y):- 
+    parent(X,Z),
+    parent(Z,Y).
+
+no_grandparent(X,Y):- cneg_tr([], grandparent(X, Y)).
