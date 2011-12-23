39047a32dc3e996e622ae22127ffe74b72f62409
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-12-23 13:22:54 +0000

    New examples.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@436 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ negation/cneg_ciao/ex_lists_ops.pl
@@ -0,0 +1,20 @@
+:- module(ex_lists_ops,_,[.(cneg), .(debugger_pkg)]).
+
+append1([],L,L).
+append1([X|L],L1,[X|R]):-
+	append1(L,L1,R).
+
+prefix(P,L):- 
+	append1(P,_S,L).
+suffix(S,L):- 
+	append1(_P,S,L).
+
+sublist([],_List).
+sublist([El|Sub],List):- 
+	prefix(P,List),
+	suffix([El|Sub],P).
+
+no_append1(X,Y,Z):-cneg_tr([], append1(X,Y,Z)).
+no_prefix(P,L):-cneg_tr([], prefix(P,L)).
+no_suffix(S,L):-cneg_tr([], suffix(S,L)).
+no_sublist(L1,L2):-cneg_tr([], sublist(L1,L2)).
