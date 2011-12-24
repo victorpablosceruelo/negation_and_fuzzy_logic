b14ac9384c3246d4d0eeb8e93834156537ac7599
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2011-12-24 11:45:17 +0000

    Let's see if this fixes    Building bootstrap engine ./ciao/engine/build_engine.sh: line 221: ./build/objs/LINUXi86_64-install/version.h-tmp: No such file or directory make: *** [build] Error 1 dpkg-buildpackage: error: debian/rules build gave error exit status 2
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@443 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- ciao-debian/patches/ciao-engine-build_engine.sh
+++ ciao-debian/patches/ciao-engine-build_engine.sh
@@ -14,3 +14,12 @@ Index: ciao/engine/build_engine.sh
  if test x"${CIAOBUILDDIR}" = x""; then
      echo "error: CIAOBUILDDIR is undefined" >&2
      echo "Please, do not call this script directly. Use 'ciaosetup' instead." >&2
+@@ -213,6 +218,8 @@
+ # affected)
+ update_file() { # FILE
+     TMP=${1}-tmp
++    TMP_PATH=`dirname ${TMP}`
++    mkdir -pv ${TMP_PATH}
+     cat > ${TMP}
+     if cmp -s ${1} ${TMP}; then
+ 	# identical (keep the original, thus preserving its
