c53f6169072f192006bcbc277a035f5284fd043d
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-03-13 10:39:19 +0000

    Fixed priority problem (caused by ciao prolog update).
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@642 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- debugger_pkg/debugger_pkg.pl
+++ debugger_pkg/debugger_pkg.pl
@@ -1,7 +1,7 @@
 :- package(debugger_pkg).
 %:- load_compilation_module(library('debugger_pkg/debugger_pkg_tr')).
 :- load_compilation_module(.(debugger_pkg_tr)).
-:- add_sentence_trans(debugger_pkg/3).
+:- add_sentence_trans(debugger_pkg/3, 760).
 % :- add_clause_trans(debugger_pkg/3).
 % :- translation_predname(debugger_pkg/3).
 
