6a7c2a99123321eaa914e12671ecd566504d0db5
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-03-14 15:52:49 +0000

    Changed compilation options.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@652 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- ciao-debian/ciao-prolog-local-build.sh
+++ ciao-debian/ciao-prolog-local-build.sh
@@ -2,40 +2,26 @@
 
 # set -x
 
-if [ -z "$1" ] || [ "$1" == "" ] || [ -z "$2" ] || [ "$2" == "" ] || [ -z "$3" ] || [ "$3" == "" ]; then
+if [ -z "$1" ] || [ "$1" == "" ]; then
 	echo " "
 	echo "This is an utility to build Ciao Prolog debian packages."
-	echo "usage: $0 VERSION SVN_REVISION_CIAO SVN_REVISION_DEBIAN_CIAO_REPOS "
-	echo "example: $0 1.13 11293 "
-	echo "example: $0 1.14.2 13646 382"
-	echo "example: $0 1.15.0 14440 latest"
+	echo "usage: $0 SVN_REVISION_CIAO "
+	echo "example: $0 14440 "
 	echo " "
 	exit 0
 else
-	VERSION="$1"
-	REVISION="$2"
-	DEBIAN_REPOS_REVISION="$3"
+	REVISION="$1"
 fi;
+
+# Compilation script.
+CIAOSETUP=./ciaosetup
 
 # Repositories urls.
 REPOS_1=svn+ssh://clip.dia.fi.upm.es/home/clip/SvnReps/Systems/CiaoDE/trunk
-REPOS_2=https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic/ciao-debian/
+# REPOS_2=https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic/ciao-debian/
 # SVNREPO_2=svn+ssh://clip.dia.fi.upm.es/home/egallego/clip/repos/ciao-debian/
 
-DATE=`date +%Y%m%d`
-PKG_VERSION=$VERSION+r$REVISION
-FOLDER_NAME=ciao-prolog-$PKG_VERSION
-FILE_NAME=ciao-prolog_$PKG_VERSION
-BUILD_TGZ=$FILE_NAME.orig.tar.gz
-BUILD_DSC=$FILE_NAME.dsc
-BUILD_DIFF=$FILE_NAME.diff
-BUILD_DIFF_GZ=$FILE_NAME.diff.gz
-
-# Where to put everything.
-pushd ~/tmp
-
-# Clean up
-rm -fv $BUILD_TGZ $BUILD_DSC $BUILD_DIFF $BUILD_DIFF_GZ
+FOLDER_NAME=~/secured/tests/CiaoDE_trunk
 
 # Ensure folder exists and has a pristine copy.
 # rm -fR $FOLDER_NAME
@@ -55,80 +41,66 @@ fi
 popd
 echo " "
 
-# Ensure folder exists.
-# rm -fR $FOLDER_NAME/debian
-mkdir -p $FOLDER_NAME/debian
-pushd $FOLDER_NAME/debian
-
 echo " "
-if [ -d .svn ]; then
-# if [ -d .git ]; then
-    if [ "$DEBIAN_REPOS_REVISION" == "latest" ]; then
-	echo "updating debian subfolder to last revision from $REPOS_2"
-	svn revert -R .
-	svn update
-    else
-	echo "updating debian subfolder to revision $DEBIAN_REPOS_REVISION from $REPOS_2"
-	svn revert -R .
-	svn update --revision $DEBIAN_REPOS_REVISION
-	# git svn rebase
-    fi
-else
-    if [ "$DEBIAN_REPOS_REVISION" == "latest" ]; then
-	echo "checking out debian subfolder to last revision from $REPOS_2"
-	svn co $REPOS_2 . 
-	# git svn clone $REPOS_2 .
-    else
-	echo "checking out debian subfolder to revision $DEBIAN_REPOS_REVISION from $REPOS_2"
-	svn co $REPOS_2 . --revision $DEBIAN_REPOS_REVISION
-    fi
-fi
-popd
+$(CIAOSETUP) clean
+echo " "
+$(CIAOSETUP) clean_config
+echo " "
+$(CIAOSETUP) realclean
+echo " "
+$(CIAOSETUP) braveclean
 echo " "
 
-pushd $FOLDER_NAME
-# Apply patches to the ciao distribution.
-echo " -> Applying patches in debian/patches to ciao distribution ..."
-for file in debian/patches/* 
-do
-	if [ ! -d $file ] && [ ! "$file" == "." ] && [ ! "$file" == ".." ]; then 
-		echo "Found file patch in $file"
-	#	patch -p0 --forward --verbose < $file
-	fi
-done
-echo " "
-popd
+# exit 0
 
-# Create .orig tarball
-echo " -> Compressing $FOLDER_NAME in file $BUILD_TGZ "
-tar cfz $BUILD_TGZ $FOLDER_NAME
+$(CIAOSETUP) configure \
+    --stop-if-error=yes \
+#    --registration_type=all \
+    --registration_type=user \
+#    --instype=global \
+    --instype=local
+#    --prefix=/usr \
+    --prefix=~/secured/local \
+    --execmode=755 
+    --datamode=644 
+#    --installgroup=root \
+#    --update_bashrc=no \
+    --update_bashrc=yes \
+    --dotbashrc=~/.bashrc \
+    --update_cshrc=no \
+    --install_prolog_name=no \
+    --install_emacs_support=yes \
+    --install_xemacs_support=no \
+    --update_dotemacs=yes \
+    --dotemacs=~/.emacs \
+    --emacsinitdir=~/.emacs.d \
+    --update_dotxemacs=no \
+    --htmldir=~/public_html/ciao-manual \
+    --htmlurl=http://localhost/ciao-manual \
+    --docdir=~/secured/local/share/doc/ciao-prolog \
+    --mandir=~/secured/local/share/man \
+    --infodir=~/secured/local/share/info \
+    --web_images_path=~/secured/local/share/doc/ciao-prolog/html \
+    --with_mysql=yes \
+    --mysql_client_directory=/usr/lib \
+    --with_gsl=no --with_ppl=no \
+    --with_java_interface=yes \
+    --with_ant=yes \
+    --optimizing_compiler=yes \
+    --use_threads=yes \
+    --use_posix_locks=no \
+    --and_parallel_execution=no \
+    --par_back=no \
+    --tabled_execution=no \
+#    --optim_level=normal \
+    --optim_level=optimized \
+    --with_chr=yes \
+    --with_ciaoppcl=no \
+    --compress_lib=no \
+    --unused_pred_warnings=yes \
+    --runtime_checks=no \
+    --set_flag_options=yes 
 
-# Create a suitable changelog entry.
-pushd $FOLDER_NAME
-dch -b -v $PKG_VERSION "Snapshot version $PKG_VERSION date $DATE."
-popd
-dpkg-source -b $FOLDER_NAME
-
-echo " "
-echo "-> It is needed for the following operation the symbolic links: "
-echo "   /etc/pbuilderrc -> pbuilderrc "
-echo "   /etc/pbuilder/pbuilderrc -> pbuilderrc "
-# echo "   /etc/pbuilder/pbuilder-apt -> pbuilder-apt "
-echo " "
-echo "-> Now run: [ the first two only if you did not execute them before ]"
-echo " sudo pbuilder --clean "
-echo " sudo cowbuilder --create "
-echo " sudo cowbuilder --update "
-echo " sudo cowbuilder --build $BUILD_DSC "
-echo " "
-
-popd
-echo " "
-echo " "
-
-set -x
-sudo cowbuilder --build $BUILD_DSC
-
-DIR=`dirname $0`
-sudo $DIR/ciao-prolog-autoinstall.sh 
+./$(CIAOSETUP) build
+./$(CIAOSETUP) docs
 
