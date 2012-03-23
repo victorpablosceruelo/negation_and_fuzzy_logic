69681d5cd849a5fae34fe31509e36a4485308cc9
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-03-23 12:00:52 +0000

    The script was erroneous.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@665 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- ciao-debian/ciao-prolog-64bits.sh
+++ ciao-debian/ciao-prolog-64bits.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+echo " "
+echo "(estos comandos ponen variables de entorno que deben"
+echo " estar siempre activas cuando llames a 'ciaosh')"
+echo " "
+echo "eval \`./ciaotool bash-env\`"
+echo " "
+pushd ~/secured/tests/CiaoDE_trunk/optim_comp 2>&1 >/dev/null
+echo "`./ciaotool bash-env`"
+popd 2>&1 >/dev/null
+echo "export ABSMACH_OPTGRP=tags"
+echo "export ABSMACH_OPTS=tagscheme28"
+echo " "
+echo "(estos comandos generan el ciao de 64 bits) "
+echo " "
+echo "ciaotool update-comp"
+echo "ciaotool update-all"
+echo " "
+
+echo "TEST: set | grep \"\(ABS\|CIAO\)\""
+echo " "
+set | grep "\(ABS\|CIAO\)"
+echo " "
+
+exit 0
+
 pushd ~/secured/tests/CiaoDE_trunk
 pushd optim_comp
 
-#(estos comandos ponen variables de entorno que deben
-#estar siempre activas cuando llames a 'ciaosh')
 eval `./ciaotool bash-env`
 export ABSMACH_OPTGRP=tags
 export ABSMACH_OPTS=tagscheme28
@@ -12,4 +35,6 @@ export ABSMACH_OPTS=tagscheme28
 ciaotool update-comp
 ciaotool update-all
 
+popd
+popd
 echo "El toplevel se arranca con 'ciaosh'."
