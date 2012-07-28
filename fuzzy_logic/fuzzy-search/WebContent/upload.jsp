6b5d73e5721993309dbcdc22d5ce78c5008111a8
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-07-28 10:22:34 +0000

    Starting ...
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@684 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ fuzzy_logic/fuzzy-search/WebContent/upload.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<FORM ENCTYPE='multipart/form-data' method='POST' action="UploadServlet">
+<INPUT TYPE='file' NAME='fuzzy-database' size="50">
+<INPUT TYPE='submit' VALUE='Upload File'>
+</FORM>
+
+</body>
+</html>
---
+++ fuzzy_logic/fuzzy-search/src/tests/HelloWorld.java
@@ -0,0 +1,52 @@
+package tests;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+
+		    PrintWriter out = response.getWriter();
+
+		    out.println("Hello, world!");
+		    out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
+
