c7fe452ecd5e537dc93a475e00b335b09b7e9588
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-04 16:54:24 +0000

    Removed dependency to struts.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@709 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/fuzzy-search/src/socialAuth/SocialAuthenticationServlet.java
+++ fuzzy_logic/fuzzy-search/src/socialAuth/SocialAuthenticationServlet.java
@@ -38,22 +38,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-/* Do not use struts !!!
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.util.RequestUtils;
-*/
 
 import socialAuth.SocialAuthConfig;
 import socialAuth.SocialAuthManager;
 import socialAuth.AuthForm;
 import socialAuth.exception.SocialAuthException;
 import socialAuth.util.SocialAuthUtil;
+
+// Do not use struts !!!
+// import org.apache.struts.action.Action;
+// import org.apache.struts.action.ActionForm;
+// import org.apache.struts.action.ActionForward;
+// import org.apache.struts.action.ActionMapping;
+// import org.apache.struts.util.RequestUtils;
+
 
 /**
  * 
