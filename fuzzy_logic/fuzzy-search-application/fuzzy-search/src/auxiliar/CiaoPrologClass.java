c4ccac7b2d61081b7fcb98eb8eda44d6a7559029
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-13 11:20:18 +0000

    Now CiaoJava-1.15.0-14580 is a package !!!
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@759 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ fuzzy_logic/fuzzy-search-application/fuzzy-search/src/auxiliar/CiaoPrologClass.java
@@ -0,0 +1,14 @@
+package auxiliar;
+
+import CiaoJava.*;
+
+public class CiaoPrologClass {
+
+	static private String pathToPlServer = "/usr/lib/ciao/ciao-1.15/library/javall/plserver";
+	static private PLConnection plServer = null;
+	
+	public CiaoPrologClass() {
+		
+	}
+
+}
