003ca4185b878c70da41f5ee76b28b3e4ce8a57f
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-16 19:42:26 +0000

    Clean up.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@807 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- ciao-pkg-for-debian/ciao-prolog-prepare-chroot-clean-build-and-install.sh
+++ ciao-pkg-for-debian/ciao-prolog-prepare-chroot-clean-build-and-install.sh
@@ -13,6 +13,7 @@ fi
 CHROOT_FOLDER="$1" 
 DEST_FOLDER="${2}"
 FULL_PATH="${CHROOT_FOLDER}/${DEST_FOLDER}/"
+COMPILATION_PATH="/usr/share/CiaoDE"
 
 # Info: DEST_FOLDER must point to $(CURDIR)/debian/tmp
 
@@ -22,13 +23,13 @@ mkdir -p ${FULL_PATH}
 
 for file in *; do
 	if [ ! -z $file ] && [ ! "$file" == "" ] && [ ! "$file" == "debian" ] && [ ! "$file" == ".svn" ]; then
-		mv -v $file ${FULL_PATH} 2>&1 >> ${FULL_PATH}/files-moved.txt
+		mv -v $file ${COMPILATION_PATH} 2>&1 >> ${FULL_PATH}/files-moved.txt
 	fi
 done
 
 # Copy the scripts we need to clean, build and install
-cp -dpRv debian/ciao-prolog-configure-clean-build-and-install.sh ${FULL_PATH}
-cp -dpRv debian/ciao-prolog-configure.sh ${FULL_PATH}
+cp -dpRv debian/ciao-prolog-configure-clean-build-and-install.sh ${COMPILATION_PATH}
+cp -dpRv debian/ciao-prolog-configure.sh ${COMPILATION_PATH}
 ls -la ${FULL_PATH}
 
 # To save configurations for emacs and bash
@@ -36,7 +37,9 @@ mkdir -p ${CHROOT_FOLDER}/root
 touch ${CHROOT_FOLDER}/root/.emacs
 touch ${CHROOT_FOLDER}/root/.bashrc
 
-/usr/sbin/chroot ${CHROOT_FOLDER} ${DEST_FOLDER}/ciao-prolog-configure-clean-build-and-install.sh
+pushd ${COMPILATION_PATH}
+debian/ciao-prolog-configure-clean-build-and-install.sh
+popd
 
 mv ${CHROOT_FOLDER}/root/.emacs ${FULL_PATH}/add_to_.emacs
 mv ${CHROOT_FOLDER}/root/.bashrc ${FULL_PATH}/add_to_.bashrc
