80d1224bbf40760f6aa9cdf0a8ebd45da0d0a533
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-16 16:35:03 +0000

    A new procedure to get a debian pkg of ciao prolog. Let's test it.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@795 f9b316aa-e3e8-4615-9afe-4ac41bce987a

---
+++ ciao-pkg-for-debian/ciao-prolog-build-and-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CIAOSETUP=ciaosetup
+
+@echo " "
+./$(CIAOSETUP) clean
+@echo " "
+./$(CIAOSETUP) clean_config
+@echo " "
+./$(CIAOSETUP) realclean
+@echo " "
+./$(CIAOSETUP) braveclean
+@echo " "
+
+# Facility to use always the same configuration
+./debian/ciao-prolog-configure.sh $(CIAOSETUP)
+
+# Add here commands to compile the package.
+./$(CIAOSETUP) build
+./$(CIAOSETUP) docs
+
+# And the commands to install it.
+./$(CIAOSETUP) install
+
---
+++ ciao-pkg-for-debian/ciao-prolog-prepare-chroot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ -z $1 ] || [ "$1" == "" ] || [ -z $2 ] || [ "$2" == "" ]; then
+	echo " "
+	echo "ERROR: This arguments needs two arguments to be run."
+	echo "${0} CHROOT_FOLDER DEST_FOLDER
+	echo " "
+	exit -1
+fi
+
+CHROOT_FOLDER="$1" 
+DEST_FOLDER="${2}"
+FULL_PATH="${CHROOT_FOLDER}/${DEST_FOLDER}/"
+
+# Info: DEST_FOLDER must point to $(CURDIR)/debian/tmp
+
+# ensure folders exist.
+mkdir -p ${CHROOT_FOLDER}
+mkdir -p ${DEST_FOLDER}
+
+for file in *; do
+	if [ ! -z $file ] && [ ! "$file" == "" ] && [ ! "$file" == "debian" ] && [ ! "$file" == ".svn" ]; then
+		mv -v $file ${FULL_PATH}
+	fi
+done
+
+# Copy the scripts in debian folder.
+cp -dpRv debian ${FULL_PATH}
+
+pushd ${FULL_PATH}
+ln -s debian/ciao-prolog-build-and-install.sh ciao-prolog-build-and-install.sh
+popd
+
+# To save configurations for emacs and bash
+mkdir -p ${CHROOT_FOLDER}/root
+touch ${CHROOT_FOLDER}/root/.emacs
+touch ${CHROOT_FOLDER}/root/.bashrc
+
+/usr/sbin/chroot ${CHROOT_FOLDER} ${DEST_FOLDER}/ciao-prolog-build-and-install.sh
+
+mv ${CHROOT_FOLDER}/root/.emacs ${FULL_PATH}/add_to_.emacs
+mv ${CHROOT_FOLDER}/root/.bashrc ${FULL_PATH}/add_to_.bashrc
+
+
