87f57029d748961e9c173e9542132b21400a99dd
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-20 10:18:23 +0000

    Fixed the repos versions that work together.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@827 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- ciao-pkg-for-debian/ciao-prolog-debian-pkg-build.sh
+++ ciao-pkg-for-debian/ciao-prolog-debian-pkg-build.sh
@@ -11,7 +11,7 @@ if [ -z "$1" ] || [ "$1" == "" ] || [ -z "$2" ] || [ "$2" == "" ] || [ -z "$3" ]
 	echo "example: $0 ~/tmp 1.13 11293 "
 	echo "example: $0 ~/tmp 1.14.2 13646 382"
 	echo "example: $0 ~/tmp 1.15.0 14440 latest"
-	echo "example: $0 ~/tmp 1.15.0 14854 latest"
+	echo "example: $0 ~/tmp 1.15.0 14854 826"
 	echo " "
 	exit 0
 else
