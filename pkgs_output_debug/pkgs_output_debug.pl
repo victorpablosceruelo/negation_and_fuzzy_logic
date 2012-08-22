6a94c781aaaeee5b667d3ef1eda5fb3927b5a94d
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-22 13:00:35 +0000

    Fixed problems.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@843 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- pkgs_output_debug/pkgs_output_debug.pl
+++ pkgs_output_debug/pkgs_output_debug.pl
@@ -1,7 +1,8 @@
-:- package(debugger_pkg).
-:- load_compilation_module(library('debugger_pkg/debugger_pkg_tr')).
-% :- load_compilation_module(.(debugger_pkg_tr)).
-:- add_sentence_trans(debugger_pkg/3, 760).
+:- package(pkgs_output_debug).
+:- load_compilation_module(library('pkgs_output_debug/pkgs_output_debug_tr')).
+% To use it when it is not a system library.
+% :- load_compilation_module(.(pkgs_output_debug_tr)).
+:- add_sentence_trans(pkgs_output_debug/3, 760).
 % :- add_clause_trans(debugger_pkg/3).
 % :- translation_predname(debugger_pkg/3).
 
--- pkgs_output_debug/test.pl
+++ pkgs_output_debug/test.pl
@@ -1,4 +1,4 @@
-:- module(test,_,[debugger_pkg]).
+:- module(test,_,[pkgs_output_debug]).
 
 test("This is just a test").
 
@@ -6,3 +6,5 @@ append([], L, L).
 append([T:More], L1, [T:L2]) :-
 	append(More, L1, L2).
 	
+
+:- define_pkgs_output_debug_file('debug_file_of_file_not_test.pl').
