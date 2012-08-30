38b71007cfc8968fab783a2e85cfd5060fed1171
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-30 19:42:37 +0000

    Fixed examples good_player.pl and good_destination.pl
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@917 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/good_destination.pl
+++ fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/good_destination.pl
@@ -6,6 +6,20 @@ city(istambul).
 city(moscow).
 city(sydney).
 
+rfuzzy_type_for(nice_weather/1, [city/1]).
+rfuzzy_default_value_for(nice_weather/1, 0.5).
+
+nice_weather(madrid) value 0.8.
+nice_weather(madrid) value 0.7.
+nice_weather(madrid) value 0.2.
+
+rfuzzy_type_for(many_sights/1, [city/1]).
+rfuzzy_default_value_for(many_sights/1, 0.2).
+
+many_sights(madrid) value 0.6.
+many_sights(istambul) value 0.7.
+many_sights(sydney) value 0.6.
+
 % Define that only cities are valid individuals for
 % the fuzzy set good_destination.
 rfuzzy_type_for(good_destination/1, [city/1]).
@@ -19,22 +33,7 @@ rfuzzy_default_value_for(good_destination/1, 0.3).
 % confidence of 0.8. Its result is a combination of 
 % how much (the truth value) he/she is swift, tall and 
 % an experienced player. 
-good_destination(Place) cred (prod,1) :~ prod((nice_weather(Place), many-sights(Place))).
-
-
-rfuzzy_type_for(nice_weather/1, [city/1]).
-rfuzzy_default_value_for(nice_weather/1, 0.5).
-
-nice_weather(madrid, 0.8).
-nice_weather(madrid, 0.7).
-nice_weather(madrid, 0.2).
-
-rfuzzy_type_for(many_sights/1, [city/1]).
-rfuzzy_default_value_for(many_sights/1, 0.2).
-
-many_sights(madrid, 0.6).
-many_sights(istambul, 0.7).
-many_sights(sydney, 0.6).
+good_destination(Place) cred (prod,1) :~ prod((nice_weather(Place), many_sights(Place))).
 
 % Queries (examples).
 
--- fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/good_player.pl
+++ fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/good_player.pl
@@ -9,6 +9,37 @@ player(deron).
 player(damian).
 player(aito).
 
+
+rfuzzy_type_for(swift/1, [player/1]).
+rfuzzy_default_value_for(swift/1,0.5).
+
+swift(john) value 1.
+swift(karl) value 0.6 .
+swift(mike) value 0.9 .
+swift(lebron) value 1.
+swift(deron) value 0.8.
+
+% Height of players in mm.
+height(john, 1700).
+height(marcus, 1780).
+height(aito, 1800).
+height(damian, 1850).
+height(karl, 1900).
+height(lebron, 1950).
+height(deron, 2000).
+
+% Tall predicate depending on the player's height.
+% OLD: tall(Player, V) :- height(Player, H), tall_func(H, V).
+tall_function :# ([ (1500, 0), (1800, 0.5), (2000, 1), (3000, 1) ]).
+rfuzzy_define_fuzzification(tall/1, height/2, tall_function/2).
+
+rfuzzy_type_for(experience/1, [player/1]).
+rfuzzy_default_value_for(experience/1, 0.1).
+
+experience(lebron) value 0.4.
+experience(deron) value 0.3.
+
+
 % Define that only player are valid individuals for
 % the fuzzy set good_player.
 rfuzzy_type_for(good_player/1, [player/1]).
@@ -23,36 +54,6 @@ rfuzzy_default_value_for(good_player/1, 0.1).
 % how much (the truth value) he/she is swift, tall and 
 % an experienced player. 
 good_player(J) cred (prod,0.8) :~ prod((swift(J), tall(J), experience(J))).
-
-
-rfuzzy_type_for(experience/1, [player/1]).
-rfuzzy_default_value_for(experience/1, 0.1).
-
-experience(lebron) value 0.4.
-experience(deron) value 0.3.
-
-
-% Tall predicate depending on the player's height.
-tall(Player, V) :- height(Player, H), tall_func(H, V).
-tall_func :# ([ (1500, 0), (1800, 0.5), (2000, 1), (3000, 1) ]).
-
-% Height of players in mm.
-height(john, 1700).
-height(marcus, 1780).
-height(aito, 1800).
-height(damian, 1850).
-height(karl, 1900).
-height(lebron, 1950).
-height(deron, 2000).
-
-rfuzzy_type_for(swift/1, [player/1]).
-rfuzzy_default_value_for(swift/1,0.5).
-
-swift(john) value 1.
-swift(karl) value 0.6 .
-swift(mike) value 0.9 .
-swift(lebron) value 1.
-swift(deron) value 0.8.
 
 % Queries (examples).
 
