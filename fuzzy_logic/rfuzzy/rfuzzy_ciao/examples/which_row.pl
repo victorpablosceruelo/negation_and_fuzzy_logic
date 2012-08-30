8f00e33494c8a0dd18219981a0dba13bbe64e8f8
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-08-30 19:42:28 +0000

    Fixed which_row example.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@914 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/which_row.pl
+++ fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/which_row.pl
@@ -5,11 +5,6 @@ rows_list(row2).
 rows_list(row3).
 rows_list(row4).
 
-rfuzzy_type_for(which_row/1, [rows_list/1]).
-rfuzzy_default_value_for(which_row/1, 0.1).
-(which_row(J) cred (luka,0.9)) :~ prod((people(J), products_people(J))).
-
-
 rfuzzy_type_for(people/1, [rows_list/1]).
 rfuzzy_default_value_for(people/1, 0.1).
 
@@ -23,3 +18,7 @@ rfuzzy_default_value_for(products_people/1, 0.5).
 
 products_people(row2) value 0.2.
 products_people(row4) value 0.8.
+
+rfuzzy_type_for(which_row/1, [rows_list/1]).
+rfuzzy_default_value_for(which_row/1, 0.1).
+(which_row(J) cred (luka,0.9)) :~ prod((people(J), products_people(J))).
