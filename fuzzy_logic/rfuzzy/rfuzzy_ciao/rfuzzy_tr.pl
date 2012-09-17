16853e1aec2bdff103ba098aa92190518cd568d1
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-09-17 11:52:49 +0000

    Instead of failing, return an error.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@965 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/rfuzzy/rfuzzy_ciao/rfuzzy_tr.pl
+++ fuzzy_logic/rfuzzy/rfuzzy_ciao/rfuzzy_tr.pl
@@ -888,22 +888,18 @@ add_auxiliar_code(Fuzzy_Rules_In, Fuzzy_Rules_Out) :-
 % ------------------------------------------------------
 
 code_for_getting_attribute_values([Code_1, Code_2]) :-
-%	Module = (:-),
-	Code_1 = (
-		     rfuzzy_var_truth_value(Var, Condition, Value) :-
+	Code_1 = (rfuzzy_var_truth_value_aux(Var, Condition, Value) :-
 		 print_msg('debug', 'rfuzzy_var_truth_value :: Var', Var),
 		 var(Var),					 
 		 dump_constraints(Var, Var, Dump), !,
 		 print_msg('debug', 'rfuzzy_var_truth_value :: dump_constraints :: Dump', Dump),
-		 rfuzzy_process_attribute_dump(Dump, Var, Condition, Value)
-		 ),
-%    dump_internal(Var, Var, [cva(Var, Value)])
-
-		 Code_2 = (
-			      rfuzzy_var_truth_value(Var, _Condition, _Value) :-
+		 rfuzzy_process_attribute_dump(Dump, Var, Condition, Value),
+		 !),
+		 Code_2 = (rfuzzy_var_truth_value_aux(Var, 'error', 0) :-
 			  print_msg('error', 'rfuzzy_var_truth_value :: Var', Var),
-			  !, fail ).
+			  !).
 
+%    dump_internal(Var, Var, [cva(Var, Value)])
 % ------------------------------------------------------
 % ------------------------------------------------------
 % ------------------------------------------------------
