d18e234c536e36a4b2cfbf317100519cc28126ee
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-10-11 17:38:09 +0000

    This should be enough.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@1026 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/flese/src/auxiliar/AnswerTermInJava.java
+++ fuzzy_logic/flese/src/auxiliar/AnswerTermInJava.java
@@ -1,8 +1,9 @@
 package auxiliar;
 
-import org.apache.commons.logging.Log;
-
+import CiaoJava.PLInteger;
 import CiaoJava.PLList;
+import CiaoJava.PLString;
+import CiaoJava.PLStructure;
 import CiaoJava.PLTerm;
 import CiaoJava.PLVariable;
 
@@ -16,38 +17,91 @@ public class AnswerTermInJava {
 	private String creationMsgs = "";
 	
 	public AnswerTermInJava(PLTerm term, PLTerm prologQueryAnswer) throws Exception{
-		
+		conversion(term, prologQueryAnswer);
+	}
+	
+	private void conversion(PLTerm term, PLTerm prologQueryAnswer) throws Exception {
+		// Log ...
+		creationMsgs += "\nTerm: " + term.toString() + " \n";
+		// For variables.
 		if (term.isVariable()) {
-			PLVariable variable = (PLVariable) term;
-			PLTerm binding = variable.getBinding();
+			PLVariable prologVariable = (PLVariable) term;
+			creationMsgs += "is a variable ";
+			PLTerm binding = prologVariable.getBinding();
 			if (binding == null) {
-				creationMsgs += "variable has no binding. ";
-				singleAnswerTerm = variable.toString();
+				creationMsgs += "without binding. ";
+				singleAnswerTerm = "-variable-";
 			}
 			else {
-				creationMsgs += binding.toString();
-				if (binding.isList()) {
-					creationMsgs += " is a list. ";
-					PLList list = (PLList) binding;
-					listLength = list.length();
-					if (listLength > 0) {
-						compositeAnswerTerm = new AnswerTermInJava [listLength];
-						for (int i=0; i<listLength; i++) {
-							compositeAnswerTerm[i] = new AnswerTermInJava(list.getHead(), prologQueryAnswer);
-							list = (PLList) list.getTail();
-						}
-					}
-				}
-				else {
-					creationMsgs += " is not a list. ";
-					singleAnswerVar = binding.toString();
+				creationMsgs += "with binding: "+binding.toString();
+				conversion(binding, prologQueryAnswer);
+			}
+		}
+		// For lists.
+		if (term.isList()) {
+			creationMsgs += "is a list. ";
+			PLList prologList = (PLList) term;
+			listLength = prologList.length();
+			if (listLength > 0) {
+				compositeAnswerTerm = new AnswerTermInJava [listLength];
+				for (int i=0; i<listLength; i++) {
+					compositeAnswerTerm[i] = new AnswerTermInJava(prologList.getHead(), prologQueryAnswer);
+					prologList = (PLList) prologList.getTail();
 				}
 			}
 		}
-		else {
-			term.toString();
+		// For integer
+		if (term.isInteger()) {
+			creationMsgs += " is an integer. ";
+			PLInteger prologInteger = (PLInteger) term;
+			singleAnswerTerm = prologInteger.toString();
+		}
+		// For float ???
+		// For atom ???
+		// For structure
+		if (term.isStructure()) {
+			creationMsgs += " is an structure. ";
+			PLStructure prologInteger = (PLStructure) term;
+			singleAnswerTerm = prologInteger.toString();			
+		}		
+		// For string
+		if (term.isString()) {
+			creationMsgs += " is an string. ";
+			PLString prologInteger = (PLString) term;
+			singleAnswerTerm = prologInteger.toString();			
+		}
+		
+		if (! (term.isVariable() || term.isList() || term.isInteger() || term.isStructure() || term.isString())) {
+			creationMsgs += " -- what the hell is this??? ";
+			singleAnswerTerm = term.toString();
 		}
 	}
 	
-	public String toString = null;
+	public String toString () {
+		String retVal = " ";
+		if (singleAnswerTerm != null) retVal += singleAnswerTerm;
+		if (compositeAnswerTerm != null) {
+			retVal += "(";
+			for (int i=0; i<listLength; i++) {
+				retVal += compositeAnswerTerm[i].toString();
+				if ((i+1) < listLength) retVal += ", ";
+			}
+			retVal += ")";
+		}
+		retVal += " ";
+		return retVal;
+	}
+	
+	public PLTerm getprologQueryAnswer() {
+		return prologQueryAnswer;
+	}
+	public String getCreationMsgs () {
+		return creationMsgs;
+	}
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
