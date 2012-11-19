5fb63c518c158eb5c6e12f348ef5a86d30b62fcc
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-11-19 08:52:45 +0000

    working on personalization
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@1223 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- fuzzy_logic/flese/src/auxiliar/FunctionsClass.java
+++ fuzzy_logic/flese/src/auxiliar/FunctionsClass.java
@@ -2,15 +2,19 @@ package auxiliar;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FunctionsClass {
 	
-	String [] result = null;
-	
-	ArrayList <ArrayList <FunctionClass>> functionsOrdered = null;
+	private ArrayList <ArrayList <FunctionClass>> functionsOrdered = null;
+	private Iterator <Iterator <FunctionClass>> functionsOrderedIterator = null;
+	private String [][] functionsOrderedInJavaScript = null;
 
 	public FunctionsClass (String filePath) throws Exception {
 		
+		functionsOrdered = null;
+		functionsOrderedIterator = null;
+		functionsOrderedInJavaScript = null;
 		functionsOrdered = new ArrayList <ArrayList <FunctionClass>>();
 		
 		if ((filePath != null) && ( ! ("".equals(filePath)))) {
@@ -28,28 +32,46 @@ public class FunctionsClass {
 		}
 	}
 	
-	private void addInItsList (FunctionClass function) {
+	private void addInItsList (FunctionClass function) throws Exception {
+		
+		if (function == null) throw new Exception("function cannot be null.");
 		
 		int i=0;
 		boolean placed = false;
+		ArrayList <FunctionClass> current = null;
 		
 		while ((i<functionsOrdered.size()) && (! placed)) {
-			
-			
-			for (i=0; i<functionsOredered.size(); i++) {
-				
+			current = functionsOrdered.get(i);
+			if ((current != null) && (current.size() > 0)) {
+				if (current.get(0).getPredDefined().equals(function.getPredDefined())) {
+					current.add(function);
+					placed = true;
+				}
 			}
+			i++;
 		}
 		if (! placed) {
-			ArrayList <FunctionClass> functions = new ArrayList <FunctionClass>();
-			functions.add(function);
-			functionsOrdered.add(functions);
+			current = new ArrayList <FunctionClass>();
+			current.add(function);
+			functionsOrdered.add(current);
+			placed = true;
 		}
+		// if (placed) functionsOrderedIterator = null; // Reject last saved result.
 	}
 	
 	
-	public String [] getResult() {
-		return result;
+	public Iterator <Iterator <FunctionClass>> getResult() {
+		if (functionsOrderedIterator == null) {
+			ArrayList <Iterator <FunctionClass>> tmp = new ArrayList <Iterator <FunctionClass>>();
+			Iterator <FunctionClass> current = null;
+			int i = 0;
+			while (i<functionsOrdered.size()) {
+				current = functionsOrdered.get(i).iterator();
+				tmp.add(current);
+			}
+			functionsOrderedIterator = tmp.iterator();
+		}
+		return functionsOrderedIterator;
 		// out.println("// line: "             + line + "\n");
 		// out.println("personalizePredInfo["+i+"]= new Array('" + 
 		//		function.getPredDefined() + "', '" + function.getPredNecessary() + "', '" + function.getPredOwner() + "', " + 
@@ -58,4 +80,23 @@ public class FunctionsClass {
 		// out.print("<br />\n");
 
 	}
+	
+	public String [] getResultInJavaScript() {
+		ArrayList <String> result = new ArrayList <String>();
+		String tmp = null;
+		FunctionClass function = null;
+		
+		if (functionsOrderedInJavaScript == null) {
+			Iterator <Iterator <FunctionClass>> iterator = getResult();
+			while (iterator.hasNext()) {
+				Iterator <FunctionClass> current = iterator.next();
+				while (current.hasNext()) {
+					function = current.next();
+					result.add(e)
+				}
+			}
+		}
+		return functionsOrderedInJavaScript;
+	}
+	
 }
