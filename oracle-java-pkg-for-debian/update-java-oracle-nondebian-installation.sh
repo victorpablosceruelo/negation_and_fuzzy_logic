e15a4b5e9e51185ce6384b097f9b60dbd7de926c
vpablos <vpablos@f9b316aa-e3e8-4615-9afe-4ac41bce987a>
2012-12-01 13:26:17 +0000

    Fixed the main script and the control file.
    git-svn-id: https://babel.ls.fi.upm.es/svn/negation_and_fuzzy_logic@1298 f9b316aa-e3e8-4615-9afe-4ac41bce987a

--- oracle-java-pkg-for-debian/update-java-oracle-nondebian-installation.sh
+++ oracle-java-pkg-for-debian/update-java-oracle-nondebian-installation.sh
@@ -3,10 +3,9 @@
 if [ -z "$1" ] || [ "$1" == "" ] || [ -z "$2" ] || [ "$2" == "" ]; then
 	echo " "
 	echo "This script installs java-oracle in your system from a tar.gz file you must download from oracle web page."
-	echo "Note: we install to  "
 	echo " "
 	echo "usage: sudo $0 oracle-java-jdk.tar.gz destiny_folder"
-	echo "example: sudo $0 jdk-7u9-linux-x64 /opt/oracle-java"
+	echo "example: sudo $0 Desktop/jdk-7u9-linux-x64.tar.gz /opt/oracle-java"
 	echo " "
 	echo "you can download oracle java from: "
 	echo " http://www.oracle.com/technetwork/java/javase/downloads/ "
@@ -24,7 +23,7 @@ fi
 JAVA_INSTALLATION_DIR="${2}"
 SUBDIR_NAME="`basename ${1} .tar.gz | sed 's/\.tar\.gz$//'`"
 FILENAME="${SUBDIR_NAME}.tar.gz"
-
+LOGFILE=".java-oracle-update.txt"
 mkdir -p ${JAVA_INSTALLATION_DIR}
 
 if [ ! -d ${JAVA_INSTALLATION_DIR} ]; then
@@ -34,7 +33,16 @@ fi
 
 pushd ${JAVA_INSTALLATION_DIR}
 echo "Removing subdirectory ${SUBDIR_NAME} in ${JAVA_INSTALLATION_DIR} "
-rm -fvR ${SUBDIR_NAME}
+if [ -f ${LOGFILE} ]; then 
+    rm -fv ${LOGFILE}
+fi
+touch ${LOGFILE}
+if [ ! -f ${LOGFILE} ]; then 
+    echo "Cannot create ${LOGFILE} file."
+    echo "Maybe you have no permissions to write?"
+    exit 0
+fi
+rm -fvR ${SUBDIR_NAME} > ${LOGFILE}
 mkdir -pv ${SUBDIR_NAME}
 popd
 
@@ -44,35 +52,35 @@ pushd ${JAVA_INSTALLATION_DIR}
 pushd ${SUBDIR_NAME}
 
 echo "Uncompressing file ${FILENAME} to ${JAVA_INSTALLATION_DIR}/${SUBDIR_NAME} "
-rm -fv .java-oracle-update-log.txt
-touch .java-oracle-update-log.txt
-chmod 777 .java-oracle-update.txt
-tar zxvf ${FILENAME} >> .java-oracle-update.txt
-chmod 444 .java-oracle-update.txt
+rm -fv ${LOGFILE}
+touch ${LOGFILE}
+chmod 777 ${LOGFILE}
+tar zxvf ${FILENAME} >> ${LOGFILE}
+chmod 444 ${LOGFILE}
 
 echo "Removing file ${FILENAME}"
 rm -fv ${FILENAME}
 
-echo -p "Setting the subfolder name to use ... "
+echo "Setting the subfolder name to use ... "
 for file in * ; do
-	if [ ! -z $file ] && [ ! "$file" == "" ] && [ ! "$file" == "." ] && [ ! "$file" == ".." ] && [ ! "$file" == "*" ]; then
+	if [ ! -z $file ] && [ ! "$file" == "" ] && [ ! "$file" == "." ] && [ ! "$file" == ".." ] && [ ! "$file" == "*" ] && [ ! "$file" == "${LOGFILE}" ]; then
 		DIRNAME="$file"
 	fi
 done
 popd
-echo "$DIRNAME"
+echo "Subfolder name: $DIRNAME"
 
 if [ -z "${DIRNAME}" ] || [ "${DIRNAME}" == "" ]; then
 	echo "ERROR: DIRNAME is not set. "
 	exit 0
 fi
 
-echo -p "Testing that everything is as expected ... "
+echo "Testing that everything is as expected ... "
 if [ -z "${SUBDIR_NAME}" ] || [ "${SUBDIR_NAME}" == "" ] || [ "${SUBDIR_NAME}" == "*" ]; then
     echo "The variable SUBDIR_NAME contains an invalid value. SUBDIR_NAME: ${SUBDIR_NAME}"
     exit 0
 fi
-if [ -z "${DIRNAME}" ] || [ "${DIRNAME}" == "" ] || [ ! "${DIRNAME}" == "*" ]; then
+if [ -z "${DIRNAME}" ] || [ "${DIRNAME}" == "" ] || [ "${DIRNAME}" == "*" ]; then
     echo "The variable DIRNAME contains an invalid value. DIRNAME: ${DIRNAME}"
     exit 0
 fi
@@ -82,13 +90,16 @@ if [ "${DIRNAME}" == "${SUBDIR_NAME}" ]; then
 fi
 
 echo "Removing old version in ${DIRNAME}"
-rm -fRv ${DIRNAME} 
+rm -fRv ${DIRNAME} >> ${LOGFILE}
 echo "Moving new version to ${DIRNAME} "
 mv -v ${SUBDIR_NAME}/${DIRNAME} .
 
 popd 
 
-echo "Updating alternatives ... "
+echo " "
+echo "Updating alternatives using the script" 
+echo "/usr/share/oracle-java-pkg-for-debian/update-java-oracle-nondebian-installation-aux.sh ... "
+echo " "
 /usr/share/oracle-java-pkg-for-debian/update-java-oracle-nondebian-installation-aux.sh ${JAVA_INSTALLATION_DIR} ${DIRNAME}
 echo "END."
 
