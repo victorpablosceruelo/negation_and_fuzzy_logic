d686b6c1a6e055d89073ada3441a9091c1383fde
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2012-12-06 13:44:50 +0100

    backup of not working jquery.form

--- fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
+++ fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
@@ -345,59 +345,7 @@ function insertFileUploadFacility(parentDivId) {
 			alert("responseText: " + responseText);
 		}
 		  
-	});
-	
-	/*
-	var uploadBar = $('#uploadBar');
-	var uploadPercent = $('#uploadPercent');
-	var uploadStatus = $('#uploadStatus');
-	   
-	$('uploadForm').ajaxForm({
-	    beforeSend: function() {
-	    	uploadStatus.empty();
-	        var percentVal = '0%';
-	        uploadBar.width(percentVal);
-	        uploadPercent.html(percentVal);
-	    },
-	    uploadProgress: function(event, position, total, percentComplete) {
-	        var percentVal = percentComplete + '%';
-	        uploadBar.width(percentVal);
-	        uploadPercent.html(percentVal);
-	    },
-		complete: function(xhr) {
-			// uploadBar.innerHTML = "";		
-			uploadStatus.html(xhr.responseText);
-		}
-	});
-	*/
-/*    var options = { 
-            target:        '#uploadStatus',   // target element(s) to be updated with server response 
-            // beforeSubmit:  showRequest,  // pre-submit callback 
-            // success:       showResponse  // post-submit callback 
-     
-            // other available options: 
-            //url:       url         // override for form's 'action' attribute 
-            //type:      type        // 'get' or 'post', override for form's 'method' attribute 
-            //dataType:  null        // 'xml', 'script', or 'json' (expected server response type) 
-            //clearForm: true        // clear all form fields after successful submit 
-            //resetForm: true        // reset the form after successful submit 
-     
-            // $.ajax options can be used here too, for example: 
-            //timeout:   3000 
-        }; 
-	
-    // bind to the form's submit event 
-    $('uploadForm').submit(function() { 
-        // inside event callbacks 'this' is the DOM element so we first 
-        // wrap it in a jQuery object and then invoke ajaxSubmit 
-        $(this).ajaxSubmit(options); 
- 
-        // !!! Important !!! 
-        // always return false to prevent standard browser submit and page navigation 
-        return false; 
-    }); 
-*/
-	
+	});	
 }
 
 function autoSendForm(formId) {
