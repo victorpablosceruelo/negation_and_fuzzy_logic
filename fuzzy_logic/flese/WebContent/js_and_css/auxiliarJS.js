4bb637839f10a8d8ec05c4d8ae11e39b9e4af3bb
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2012-12-06 12:17:43 +0100

    Cannot make work form plugin ... :-(

--- fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
+++ fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
@@ -303,7 +303,8 @@ function insertFileUploadFacility(parentDivId) {
 						  "<INPUT TYPE='file' NAME='programFileToUpload' size='50' onchange='autoSendForm(\"uploadForm\");'>" +
 						  "<DIV id='uploadButton'> </DIV> " +
 						  "</FORM>" +
-						  "<div id='uploadBar'></div ><div id='uploadPercent'>0%</div ></div><div id='uploadStatus'></div>";
+						  "<div id='uploadBar'></div ><div id='uploadPercent'>0%</div ></div><div id='uploadStatus'></div>" + 
+						  "<iframe name='submit-iframe' style='display: none;'></iframe>";
     	
 	var uploadBar = $('#uploadBar');
 	var uploadPercent = $('#uploadPercent');
@@ -322,9 +323,38 @@ function insertFileUploadFacility(parentDivId) {
 	        uploadPercent.html(percentVal);
 	    },
 		complete: function(xhr) {
+			// uploadBar.innerHTML = "";		
 			uploadStatus.html(xhr.responseText);
 		}
-	}); 
+	});
+	
+/*    var options = { 
+            target:        '#uploadStatus',   // target element(s) to be updated with server response 
+            // beforeSubmit:  showRequest,  // pre-submit callback 
+            // success:       showResponse  // post-submit callback 
+     
+            // other available options: 
+            //url:       url         // override for form's 'action' attribute 
+            //type:      type        // 'get' or 'post', override for form's 'method' attribute 
+            //dataType:  null        // 'xml', 'script', or 'json' (expected server response type) 
+            //clearForm: true        // clear all form fields after successful submit 
+            //resetForm: true        // reset the form after successful submit 
+     
+            // $.ajax options can be used here too, for example: 
+            //timeout:   3000 
+        }; 
+	
+    // bind to the form's submit event 
+    $('uploadForm').submit(function() { 
+        // inside event callbacks 'this' is the DOM element so we first 
+        // wrap it in a jQuery object and then invoke ajaxSubmit 
+        $(this).ajaxSubmit(options); 
+ 
+        // !!! Important !!! 
+        // always return false to prevent standard browser submit and page navigation 
+        return false; 
+    }); 
+*/
 	
 }
 
