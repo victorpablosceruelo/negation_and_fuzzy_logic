1b589b4cde3a198cac11d4026241e905480ab192
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2012-12-08 14:21:05 +0100

    queryResult -> queryAnswer

--- fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
+++ fuzzy_logic/flese/WebContent/js_and_css/auxiliarJS.js
@@ -198,7 +198,7 @@ function insertQuerySelection(parentDivId, runQueryDivId, selectQueryDivId, file
 			eval(responseHtmlText);
 			
 			// Show the answers retrieved to the user.
-			showAnswers(runQueryDivId);
+			showQueryAnswers(runQueryDivId);
 		}
 		  
 	});
@@ -1139,29 +1139,33 @@ function resultOver(value, index) {
 /* ---------------------------------------------------------------------------------------------------------------- */
 /* ---------------------------------------------------------------------------------------------------------------- */
 
-function showAnswers(runQueryDivId) {
+function showQueryAnswers(runQueryDivId) {
+	
+	// Initialize.
+	
+	
 	if (answers.length > 1) answers.sort(arraySortFunction);
 
 	if ((answers.length == 1) || (answers.length == 0)) {
-		document.getElementById('queryResultsBest10').innerHTML = "no answers";
-		document.getElementById('queryResultsOver70').innerHTML = "no answers";
-		document.getElementById('queryResultsOver50').innerHTML = "no answers";
-		document.getElementById('queryResultsOver0').innerHTML = "no answers";
-		document.getElementById('queryResultsAll').innerHTML = "no answers";
+		document.getElementById('queryAnswersBest10').innerHTML = "no answers";
+		document.getElementById('queryAnswersOver70').innerHTML = "no answers";
+		document.getElementById('queryAnswersOver50').innerHTML = "no answers";
+		document.getElementById('queryAnswersOver0').innerHTML = "no answers";
+		document.getElementById('queryAnswersAll').innerHTML = "no answers";
 	}
 	else {
-		createTable('queryResultsBest10');
-		createTable('queryResultsOver70');
-		createTable('queryResultsOver50');
-		createTable('queryResultsOver0');
-		createTable('queryResultsAll');
+		createTable('queryAnswersBest10');
+		createTable('queryAnswersOver70');
+		createTable('queryAnswersOver50');
+		createTable('queryAnswersOver0');
+		createTable('queryAnswersAll');
 	
 		for (var i=0; i<answers.length; i++) {
-			if ((i <= 10) && (resultOver(0, i))) insertAnswerToTable('queryResultsBest10', i);
-			if (resultOver(0.7, i)) insertAnswerToTable('queryResultsOver70', i);
-			if (resultOver(0.5, i)) insertAnswerToTable('queryResultsOver50', i);
-			if (resultOver(0, i)) insertAnswerToTable('queryResultsOver0', i);
-			insertAnswerToTable('queryResultsAll', i);
+			if ((i <= 10) && (resultOver(0, i))) insertAnswerToTable('queryAnswersBest10', i);
+			if (resultOver(0.7, i)) insertAnswerToTable('queryAnswersOver70', i);
+			if (resultOver(0.5, i)) insertAnswerToTable('queryAnswersOver50', i);
+			if (resultOver(0, i)) insertAnswerToTable('queryAnswersOver0', i);
+			insertAnswerToTable('queryAnswersAll', i);
 		}
 	}
 }
@@ -1170,7 +1174,7 @@ function createTable(divId) {
 	var div = document.getElementById(divId);
 	var table = document.createElement('table');
 	table.id = divId + 'Table';
-	table.className = 'queryResults';
+	table.className = 'queryAnswers';
 	div.appendChild(table);
 }
 
@@ -1178,19 +1182,19 @@ function insertAnswerToTable(divId, index) {
 	var tableId = divId + 'Table';
 	var table = document.getElementById(tableId);
 	var row = table.insertRow(-1);
-	row.className = 'queryResults';
+	row.className = 'queryAnswers';
 	var cell = null;
 	var answer = answers[i];
 	for (var j=1; j<answer.length; j++) {
 		if (index==0) {
 			cell = document.createElement('th');
-			cell.className = 'queryResults';
+			cell.className = 'queryAnswers';
 			cell.innerHTML = answer[j];
 			row.appendChild(cell);
 		}
 		else {
 			cell = row.insertCell(-1);
-			cell.className = 'queryResults';
+			cell.className = 'queryAnswers';
 			if (j+1 < answer.length) {
 				cell.innerHTML = answer[j];
 			}
@@ -1214,40 +1218,40 @@ function insertAnswerToTable(divId, index) {
 		</ul>
 		
 		<div id="tabs-1">
-			<div id="queryResultsBest10"></div>
+			<div id="queryAnswersBest10"></div>
 		</div>
 		<div id="tabs-2">
-			<div id="queryResultsOver70"></div>
+			<div id="queryAnswersOver70"></div>
 		</div>
 		<div id="tabs-3">
-			<div id="queryResultsOver50"></div>
+			<div id="queryAnswersOver50"></div>
 		</div>
 		<div id="tabs-4">
-			<div id="queryResultsOver0"></div>
+			<div id="queryAnswersOver0"></div>
 		</div>
 		<div id="tabs-5">
-			<div id="queryResultsAll"></div>
+			<div id="queryAnswersAll"></div>
 		</div>
 		
 	
     	<div id="tabs-6">
 				<h3>Debug information about the query run in the Prolog system</h3>
-				<table class='queryResults'>
-					<tr class='queryResults'>
-						<th class='queryResults'>Query Format</th>
-						<th class='queryResults'>Query</th>
+				<table class='queryAnswers'>
+					<tr class='queryAnswers'>
+						<th class='queryAnswers'>Query Format</th>
+						<th class='queryAnswers'>Query</th>
 					</tr>
-					<tr class='queryResults'>
-						<td class='queryResults'>Basic</td>
-						<td class='queryResults'> <%=(String) request.getAttribute("querySimpleInfoString") %> </td>
+					<tr class='queryAnswers'>
+						<td class='queryAnswers'>Basic</td>
+						<td class='queryAnswers'> <%=(String) request.getAttribute("querySimpleInfoString") %> </td>
 					</tr>
-					<tr class='queryResults'>
-						<td class='queryResults'>Complex</td>
-						<td class='queryResults'><%=request.getAttribute("queryComplexInfoString") %></td>
+					<tr class='queryAnswers'>
+						<td class='queryAnswers'>Complex</td>
+						<td class='queryAnswers'><%=request.getAttribute("queryComplexInfoString") %></td>
 					</tr>
-					<tr class='queryResults'>
-						<td class='queryResults'>Prolog</td>
-						<td class='queryResults'><%=connection.getLatestEvaluatedQuery() %> </td>
+					<tr class='queryAnswers'>
+						<td class='queryAnswers'>Prolog</td>
+						<td class='queryAnswers'><%=connection.getLatestEvaluatedQuery() %> </td>
 					</tr>
 				</table>
 		</div>
