726b1aac87408b8c4da55c6e3d2669e53b983cda
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-01-14 11:08:56 +0100

    This tests show that not(very(expensive)) and not_very_expensive have the same meaning for rfuzzy. It is not the same for FleSe: we have a bug.

---
+++ fuzzy_logic/rfuzzy/rfuzzy_ciao/examples/tests.pl
@@ -0,0 +1,30 @@
+
+:- use_module(db_leisure,_).
+
+t1(R, TV) :- assertLocalUserName('Testing_User_at_localhost_localnet'),(restaurant(_12, _13, _14, _15, _16, _17, _18), =(R, restaurant(_12, _13, _14, _15, _16, _17, _18))), not_very_expensive(R, TV), rfuzzy_var_truth_value(R, _22, _21), rfuzzy_var_truth_value(_12, _24, _23), rfuzzy_var_truth_value(_13, _26, _25), rfuzzy_var_truth_value(_14, _28, _27), rfuzzy_var_truth_value(_15, _30, _29), rfuzzy_var_truth_value(_16, _32, _31), rfuzzy_var_truth_value(_17, _34, _33), rfuzzy_var_truth_value(_18, _36, _35), rfuzzy_var_truth_value(TV, _38, _37).
+
+t2(R, TV) :- assertLocalUserName('Testing_User_at_localhost_localnet'),(restaurant(_12, _13, _14, _15, _16, _17, _18), =(R, restaurant(_12, _13, _14, _15, _16, _17, _18))), fnot(very(expensive(R, _A), _B), TV), rfuzzy_var_truth_value(R, _22, _21), rfuzzy_var_truth_value(_12, _24, _23), rfuzzy_var_truth_value(_13, _26, _25), rfuzzy_var_truth_value(_14, _28, _27), rfuzzy_var_truth_value(_15, _30, _29), rfuzzy_var_truth_value(_16, _32, _31), rfuzzy_var_truth_value(_17, _34, _33), rfuzzy_var_truth_value(_18, _36, _35), rfuzzy_var_truth_value(TV, _38, _37).
+
+t3(R, TV1, TV2) :- 
+	(restaurant(_12, _13, _14, _15, _16, _17, _18), =(R, restaurant(_12, _13, _14, _15, _16, _17, _18))), 
+	 t3_aux(R, TV1, TV2).
+
+t3_aux(R, TV1, TV2) :- 
+	(
+	     (
+		 t1(R, TV1), 
+		 t2(R, TV2), !
+	     )
+	 ;
+	     (
+		 t1(R, TV1), !
+	     )
+	 ;
+	     (
+		 t2(R, TV2), !
+	     )
+	 ;
+	     (
+		 true
+	     )
+	 ).
