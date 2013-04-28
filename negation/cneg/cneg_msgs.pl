d3f3adef2545510a51504a42a5f961fecb943dbe
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-04-28 21:22:37 +0200

    echo msgs changed ... a big problem !!

--- negation/cneg/cneg_msgs.pl
+++ negation/cneg/cneg_msgs.pl
@@ -1,7 +1,7 @@
 
 :- module(cneg_msgs,
 	[
-	    echo_msg/5, echo_msg_2pm/6, echo_msg_3pm/7,
+	    echo_msg/5, 
 	    get_trace_status_list/2,
 	    generate_empty_trace/1, end_trace/1,
 	    add_predicate_to_trace/3,
@@ -20,13 +20,123 @@
 :- multifile file_debug_is_activated/1.
 
 :- comment(title, "Auxiliary predicates for printing msgs.").
-
 :- comment(author, "V@'{i}ctor Pablos Ceruelo").
-
 :- comment(summary, "This module offers some predicates needed to print msgs.").
 
-
 :- dynamic defined_stream_to_file/2.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+echo_msg(FI, FI, Mode, Pre_Msg, Msg) :- 
+	nonvar(FI), 
+	echo_msg_aux(FI, Mode, Pre_Msg, Msg).
+
+echo_msg(FI_1, FI_2, Mode, Pre_Msg, Msg) :- 
+	nonvar(FI_1), nonvar(FI_2),
+	FI_1 \== FI_2,
+	echo_msg_aux(FI_1, Mode, Pre_Msg, Msg),
+	echo_msg_aux(FI_2, Mode, Pre_Msg, Msg).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+echo_msg_aux(0, _Mode, _Pre_Msg, _Msg) :- !. % No debugging.
+
+echo_msg_aux(FI, 'nl', _Pre_Msg, _Msg) :-
+	echo_msg_real(FI, '\n'), !.
+
+echo_msg_aux(FI, 'aux', Pre_Msg, Msg) :-
+	echo_msg_real(FI, Pre_Msg),
+	echo_msg_real(FI, Msg).
+
+echo_msg_aux(FI, 'statistics', Pre_Msg, Msg) :-
+	echo_msg_aux(FI, '', Pre_Msg, Msg),
+	echo_msg_statistics(FI), !.
+
+echo_msg_aux(FI, 'separation', _Pre_Msg, _Msg) :-
+	echo_msg_aux(FI, 'nl', '', ''),
+	echo_msg_aux(FI, '', '', '-----------------------------------------------'),
+	echo_msg_aux(FI, '', '', '-----------------------------------------------'),
+	echo_msg_aux(FI, 'nl', '', '').
+
+echo_msg_aux(FI, 'trace', Pre_Msg, Msg) :-
+	echo_msg_trace(FI, Pre_Msg, Msg).
+
+echo_msg_aux(FI, '', Pre_Msg, Msg) :-
+	  echo_msg_normal(FI, Pre_Msg, Msg).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+logo(0, '') :- !.
+logo(1, '') :- !.
+logo(_Any, '% ').
+
+echo_msg_normal(FI, Pre_Msg, Msg) :-
+	echo_msg_normal_pre_msg(FI, Pre_Msg),
+	echo_msg_normal_aux(FI, Pre_Msg, Msg).
+
+echo_msg_normal_pre_msg(_FI, []) :- !.
+echo_msg_normal_pre_msg(_FI, '') :- !.
+echo_msg_normal_pre_msg(FI, [Pre_Msg_1 | Pre_Msgs]) :-
+	echo_msg_normal_pre_msg(FI, Pre_Msg_1),
+	echo_msg_normal_pre_msg_list(FI, Pre_Msgs).
+
+echo_msg_normal_pre_msg(FI, Pre_Msg) :-
+	logo(FI, Logo),
+	echo_msg_real(FI, Logo),
+	echo_msg_real(FI, Pre_Msg),
+	echo_msg_real(FI, ' :: '), !.
+
+echo_msg_normal_pre_msg_list(_FI, []) :- !.
+echo_msg_normal_pre_msg_list(FI, [Pre_Msg | Pre_Msgs]) :- !,
+	echo_msg_real(FI, Pre_Msg),
+	echo_msg_real(FI, ' :: '), !,
+	echo_msg_normal_pre_msg_list(FI, Pre_Msgs).
+
+echo_msg_normal_aux(_FI, _Pre_Msg, '') :- !.
+echo_msg_normal_aux(FI, _Pre_Msg, []) :- echo_msg_real(FI, '[]').
+echo_msg_normal_aux(FI,   Pre_Msg, [Msg_1 | Msgs]) :- !,
+	echo_msg_normal_aux(FI, Pre_Msg, Msg_1),
+	echo_msg_normal(FI, Pre_Msg, Msgs).
+echo_msg_normal_aux(FI, _Pre_Msg, Msg) :- !,
+	echo_msg_real(FI, Msg),
+	echo_msg_aux(FI, 'nl', '', '').
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+echo_msg_statistics(0) :- !. % No backtracking, please.
+echo_msg_statistics(1) :- !,
+	nl, nl, statistics, nl, !.
+echo_msg_statistics(Any) :-
+	current_output(StdOut_Stream), % Save stdout stream.
+	get_stream_to_file(Any, Statistics_Stream),
+	set_output(Statistics_Stream), % Redirect stdout to stream.
+	nl, nl, statistics, nl, !, % Write statistics to file.
+	set_output(StdOut_Stream), % Recover stdout stream.
+	!. % No backtracking, please.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+echo_msg_trace(Any, Pre_Msg, Trace) :-
+	echo_msg_aux(Any, 'separation', '', ''),
+	echo_msg_aux(Any, 'nl', '', ''),
+	echo_msg_aux(Any, '', Pre_Msg, 'TRACE: '),
+	get_trace_status_list(Trace, Trace_Status_List),
+	echo_msg_aux(Any, 'list', '', Trace_Status_List),
+	!.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Meaning of values for variable Echo_Level:
 %
@@ -38,27 +148,24 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-echo_msg_aux(0, _File_Name, _Msg) :- !.
-echo_msg_aux(1, File_Name, Msg) :-
-	echo_msg_aux(2, File_Name, Msg),
+echo_msg_real(0, _Msg) :- !.
+echo_msg_real(1, Msg) :- !,
 	write(Msg).
-echo_msg_aux(2, File_Name, Msg) :-
-	file_debug_is_activated(true),
-	get_stream_to_file(File_Name, Stream_1),
-	write(Stream_1, Msg),
-	get_stream_to_file('with_all_debug_msgs', Stream_2),
-	write(Stream_2, Msg).
-echo_msg_aux(2, _File_Name, _Msg) :-
-	(
-	    (
-		file_debug_is_activated(Var),
-		Var \== true
-	    )
-	;
-	    (
-		\+ file_debug_is_activated(_Var)
-	    )
-	).
+echo_msg_real(Any, Msg) :- 
+	file_debug_is_activated(true), !,
+	get_file_name(Any, File_Name),
+	get_stream_to_file(File_Name, Stream),
+	write(Stream, Msg).
+echo_msg_real(_Any, _Msg) :- !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% get_file_name(Any, File_Name),
+get_file_name(2, 'debug_pkg_cneg_file_cneg_tr.pl').
+get_file_name(3, 'debug_pkg_cneg_file_cneg_rt.pl').
+get_file_name(3, 'debug_pkg_cneg_file_statistics.pl').
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -71,184 +178,10 @@ get_stream_to_file(File_Name_Atom, _Stream) :-
 get_stream_to_file(File_Name_Atom, Stream) :-
 	defined_stream_to_file(File_Name_Atom, Stream), !.
 get_stream_to_file(File_Name_Atom, Stream) :-
-	name(File_Name_Atom, File_Name_String_1), % Convert atom to string.
-	append("debug_pkg_cneg_file_", File_Name_String_1, File_Name_String_2),
-	append(File_Name_String_2, ".pl", File_Name_String_3),
-	name(FN_Out, File_Name_String_3),	% Convert string to atom.
+	name(File_Name_Atom, _File_Name_String),	% Convert string to atom.
 %	open(FN_Out,write,Stream), % This empties the file !!!
-	open(FN_Out, append, Stream),
-	assertz_fact(defined_stream_to_file(File_Name_Atom, Stream)), !,
-	echo_howto_information(2, File_Name_Atom).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% echo_msg(Echo_Level, Mode, File_Name, Pre_Msg, Msg).
-echo_msg(0, _Mode, _File_Name, _Pre_Msg, _Msg) :- !. % No debugging.
-
-echo_msg(Echo_Level, 'aux', File_Name, Pre_Msg, Msg) :- !,
-	echo_msg_aux(Echo_Level, File_Name, Pre_Msg),
-	echo_msg_aux(Echo_Level, File_Name, Msg).
-
-echo_msg(Echo_Level, 'list', File_Name, Pre_Msg, Msg) :-
-	(   (echo_msg_list(Echo_Level, File_Name, 1, Pre_Msg, Msg), !)
-	;
-	    (echo_msg(Echo_Level, 'normal', File_Name, Pre_Msg, Msg), !)
-	).
-
-echo_msg(1, 'logo', File_Name, Pre_Msg, Msg) :- !, % No logo to stdout.
-	echo_msg(2, 'logo', File_Name, Pre_Msg, Msg).
-echo_msg(Echo_Level, 'logo', File_Name, _Pre_Msg, _Msg) :-
-	echo_msg_aux(Echo_Level, File_Name, '% '), 
-	echo_msg_aux(Echo_Level, File_Name, File_Name), 
-	echo_msg_aux(Echo_Level, File_Name, ' :: '), 
-	!.
-
-echo_msg(Echo_Level, 'statistics', File_Name, _Pre_Msg, Msg) :-
-	echo_msg_statistics(Echo_Level, File_Name, Msg), !.
-
-echo_msg(Echo_Level, 'nl', File_Name, _Pre_Msg, _Msg) :-
-	echo_msg_aux(Echo_Level, File_Name, '\n'), !.
-
-echo_msg(Echo_Level, 'separation', File_Name, _Pre_Msg, _Msg) :-
-	echo_msg_separation(Echo_Level, File_Name).	
-
-echo_msg(Echo_Level, 'trace', File_Name, Pre_Msg, Msg) :-
-	echo_msg_trace(Echo_Level, File_Name, Pre_Msg, Msg).
-
-echo_msg(Echo_Level, Mode, File_Name, Pre_Msg, Msg) :-
-	  echo_msg_3pm(Echo_Level, Mode, File_Name, Pre_Msg, '', '', Msg).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-echo_msg_2pm(Echo_Level, Mode, File_Name, Pre_Msg_1, Pre_Msg_2, Msg) :-
-	echo_msg_3pm(Echo_Level, Mode, File_Name, Pre_Msg_1, Pre_Msg_2, '', Msg).
-
-echo_msg_3pm(Echo_Level, Mode, File_Name, Pre_Msg_1, Pre_Msg_2, Pre_Msg_3, Msg) :-
-	(Mode \== 'aux'), (Mode \== 'list'), (Mode \== 'logo'), 
-	 (Mode \== 'statistics'), (Mode \== 'nl'), (Mode \== 'separation'), !,
-	echo_msg(Echo_Level, 'logo', File_Name, Pre_Msg_1, Msg),
-	echo_msg_aux(Echo_Level, File_Name, Pre_Msg_1),
-	echo_msg_aux(Echo_Level, File_Name, Pre_Msg_2),
-	echo_msg_aux(Echo_Level, File_Name, Pre_Msg_3),
-	echo_msg_aux(Echo_Level, File_Name, ' :: '),
-	echo_msg_aux(Echo_Level, File_Name, Msg),
-	echo_msg(Echo_Level, 'nl', File_Name, '', ''), 
-	!.
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%echo_msg_is_a_list([]).
-%echo_msg_is_a_list([_Elto|_List]).
-
-echo_msg_list(Echo_Level, File_Name, Index, Pre_Msg, []) :-
-	echo_msg_list_aux(Echo_Level, File_Name, Index, Pre_Msg, []),
-	!. % No backtracking allowed.
-echo_msg_list(Echo_Level, File_Name, Index, Pre_Msg, [Msg|Msgs]) :- 
-	!, % No backtracking allowed.
-	echo_msg_list_aux(Echo_Level, File_Name, Index, Pre_Msg, Msg),
-	NewIndex is Index +1,
-	echo_msg_list(Echo_Level, File_Name, NewIndex, Pre_Msg, Msgs).
-
-echo_msg_list_aux(Echo_Level, File_Name, Index, Pre_Msg, Msg) :-
-	echo_msg(Echo_Level, 'logo', File_Name, Pre_Msg, Msg),
-	echo_msg_aux(Echo_Level, File_Name, Pre_Msg),
-	echo_msg_aux(Echo_Level, File_Name, ' #'),
-	echo_msg_aux(Echo_Level, File_Name, Index),
-%	echo_msg_aux(Echo_Level, File_Name, ')'),
-	echo_msg_aux(Echo_Level, File_Name, ' :: '),
-	echo_msg_aux(Echo_Level, File_Name, Msg),
-	echo_msg(Echo_Level, 'nl', File_Name, '', '').
-			
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%echo_msg_nl(0, _File_Name) :- !.
-%echo_msg_nl(1, File_Name) :-
-%	echo_msg_nl(2, File_Name),
-%	nl.
-%echo_msg_nl(2, File_Name) :-
-%	get_stream_to_file(File_Name, Stream_1),
-%	nl(Stream_1),
-%	get_stream_to_file('with_all_debug_msgs', Stream_2),
-%	nl(Stream_2).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-echo_msg_separation(Echo_Level, File_Name) :-
-	echo_msg(Echo_Level, 'nl', File_Name, '', ''), 
-	echo_msg_separation_aux(Echo_Level, File_Name), 
-	echo_msg_separation_aux(Echo_Level, File_Name),
-	echo_msg(Echo_Level, 'nl', File_Name, '', '').
-
-echo_msg_separation_aux(Echo_Level, File_Name) :-
-	echo_msg(Echo_Level, 'logo', File_Name, '', ''), 
-	echo_msg_aux(Echo_Level, File_Name, '-----------------------------------------------'),
-	echo_msg_aux(Echo_Level, File_Name, '-----------------------------------------------'),
-	echo_msg(Echo_Level, 'nl', File_Name, '', '').
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-echo_msg_statistics(0, _File_Name, _Msg) :- !. % No backtracking, please.
-echo_msg_statistics(1, File_Name, Msg) :-
-	echo_msg_statistics_aux(Msg),
-	echo_msg_statistics(2, File_Name, Msg),
-	!. % No backtracking, please.
-echo_msg_statistics(2, File_Name, Msg) :-
-	echo_msg_statistics_file(2, File_Name, Msg),
-	echo_msg_statistics_file(2, 'statistics', Msg).
-
-echo_msg_statistics_file(2, File_Name, Msg) :-
-	current_output(StdOut_Stream), % Save stdout stream.
-	get_stream_to_file(File_Name, Statistics_Stream),
-	set_output(Statistics_Stream), % Redirect stdout to stream.
-	echo_msg_statistics_aux(Msg),
-	set_output(StdOut_Stream), % Recover stdout stream.
-	!. % No backtracking, please.
-
-echo_msg_statistics_aux(Msg) :-
-	nl, write(Msg), nl, nl, 
-	statistics, !. % Write statistics to file.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-echo_msg_trace(Echo_Level, File_Name, Pre_Msg, Trace) :-
-	echo_msg(Echo_Level, 'separation', File_Name, '', ''),
-	echo_msg(Echo_Level, 'nl', File_Name, '', ''),
-	get_trace_status_list(Trace, Trace_Status_List),
-	echo_msg(Echo_Level, '', File_Name, Pre_Msg, 'TRACE: '),
-	echo_msg(Echo_Level, 'list', File_Name, '', Trace_Status_List),
-	!.
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-echo_howto_information(Echo_Level, File_Name) :-
-%	echo_msg_separation(Echo_Level, File_Name),
-	echo_msg(Echo_Level, 'nl', File_Name, '', ''),
-	echo_msg(Echo_Level, '', File_Name, 'Info for debugging', ' '),
-	echo_msg(Echo_Level, 'nl', File_Name, '', ''),
-	echo_msg(Echo_Level, '', File_Name, 'Basic frontier', 'frontier(Goal, Head, Body, FrontierTest)'),
-	echo_msg(Echo_Level, '', File_Name, 'frontier_E_IE_NIE', 'frontier_E_IE_NIE(E, IE, NIE)'),
-	echo_msg(Echo_Level, '', File_Name, 'frontier_E_IE_NIE_ied', 'frontier_E_IE_NIE_ied(E, IE_Imp, IE_Exp, IE_Dumb, NIE_Imp, NIE_Exp, NIE_Dumb)'),
-	echo_msg(Echo_Level, '', File_Name, 'Vars_Info', 'vars_info(GoalVars, UQV, ImpVars, ExpVars, RelVars, UQ_to_EQ_Vars, Dumb_Vars)'),
-%	echo_msg_separation(Echo_Level, File_Name),
-	echo_msg(Echo_Level, 'nl', File_Name, '', '').
+	open(File_Name_Atom, append, Stream),
+	assertz_fact(defined_stream_to_file(File_Name_Atom, Stream)), !.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
