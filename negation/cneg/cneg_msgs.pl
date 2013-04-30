00f2b1c75a4fa6490fa2fd4a71bb732b6a19026b
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-04-30 20:24:16 +0200

    fixed bug

--- negation/cneg/cneg_msgs.pl
+++ negation/cneg/cneg_msgs.pl
@@ -163,9 +163,9 @@ print_msg_real(_Any, _Msg) :- !.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % get_file_name(Any, File_Name),
-get_file_name(2, 'debug_pkg_cneg_file_cneg_tr.pl').
-get_file_name(3, 'debug_pkg_cneg_file_cneg_rt.pl').
-get_file_name(3, 'debug_pkg_cneg_file_statistics.pl').
+get_file_name(2, 'debug_pkg_cneg_tr.pl').
+get_file_name(3, 'debug_pkg_cneg_rt.pl').
+get_file_name(4, 'debug_pkg_cneg_statistics.pl').
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
