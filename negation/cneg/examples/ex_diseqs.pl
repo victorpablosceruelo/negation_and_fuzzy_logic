66c55bf9e8bc3b906f1962745b21d1e867931d0b
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-01 12:30:25 +0200

    Fixed example.

--- negation/cneg/examples/ex_diseqs.pl
+++ negation/cneg/examples/ex_diseqs.pl
@@ -4,44 +4,62 @@
 cneg_ignores_preds([tests/0, logo/1, tests_fail/2, tests_succeed/2]).
 
 tests :- 
-	cneg_diseq_echo(1, 3, 'nl', '', ''),
-	cneg_diseq_echo(1, 3, '', 'Tests that should fail:', ''), 
-	tests_fail(T1, T2), 
-	cneg_diseq_echo(1, 3, '', test_result(T1, T2), ''), 
-	cneg_diseq_echo(1, 3, '', 'ERROR', '').
+	print_msg(1, 3, 'nl', '', ''),
+	print_msg(1, 3, 'aux', 'Tests that should fail:', ''), 
+	print_msg(1, 3, 'nl', '', ''),
+	tests_fail(Logo, Vars, First_Part, Second_Part), 
+	test_execution(Logo, Vars, First_Part, Second_Part),
+	print_msg(1, 3, '', 'ERROR', '').
 
 tests :- 
-	cneg_diseq_echo(1, 3, 'nl', '', ''),
-	cneg_diseq_echo(1, 3, 'nl', '', ''),
-	cneg_diseq_echo(1, 3, '', 'Tests that should succeed:', ''),
-	tests_succeed(T1, T2), 
-	cneg_diseq_echo(1, 3, 'aux', test_result(T1, T2), ''), 
-	cneg_diseq_echo(1, 3, 'aux', ' : ok', ''), 
-	cneg_diseq_echo(1, 3, 'nl', '', ''), 
-	fail.
-tests :- 	
-	cneg_diseq_echo(1, 3, 'nl', '', ''),
-	cneg_diseq_echo(1, 3, '', 'if test_result(Whatever) :ok not shown there is an ERROR.', '').
+	print_msg(1, 3, 'nl', '', ''),
+	print_msg(1, 3, 'nl', '', ''),
+	print_msg(1, 3, 'aux', 'Tests that should succeed:', ''),
+	print_msg(1, 3, 'nl', '', ''),
+	tests_succeed(Logo, Vars, First_Part, Second_Part), 
+	test_execution(Logo, Vars, First_Part, Second_Part),
+	print_msg(1, 3, 'aux', ' : OK', ''), 
+	print_msg(1, 3, 'nl', '', ''), 
+	!, fail.
 
-logo(Text) :- cneg_diseq_echo(1, 3, 'aux', Text, '  ').
-just_nl :- 	cneg_diseq_echo(1, 3, 'nl', '', '').
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-tests_fail(T1, T2) :- logo('f01'), T1 = a, T2 = b, equality(T1, T2, []).
-tests_fail(T1, T2) :- logo('f02'), T1 = a, T2 = a, disequality(T1, T2, []).
-tests_fail(T1, T2) :- logo('f03'), equality(T1, T2, []), disequality(T1, T2, []).
-tests_fail(T1, T2) :- logo('f04'), disequality(T1, T2, []), just_nl, 
-	cneg_diseq_echo(1, 3, '', f04_intermediate(T1, T2), ''), nl, equality(T1, T2, []).
-tests_fail(T1, T2) :- logo('f05'), equality(T2, s(T1), []), equality(T1, T2, []).
-tests_fail(T1, T2) :- logo('f06'), equality(T1, T2, [T2]).
-tests_fail(T1, T2) :- logo('f07'), equality(T1, s(T3), []), equality(T1, T2, [T3]).
-tests_fail(T1, T2) :- logo('f08'), equality(T1, s(a), []), equality(T1, T2, [T2]).
-tests_fail(T1, T2) :- logo('f09'), equality(T1, s(_T3), []), equality(T1, T2, [T2]).
-tests_fail(T1, T2) :- logo('f10'), disequality(T1, T2, [T2]).
+test_execution(Logo, Vars, First_Part, Second_Part) :-
+	print_msg(1, 3, 'aux', Logo, ' Vars: '),
+	print_vars_diseqs(1, '', Vars), nl,
+	print_msg(1, 3, 'aux', '1st: ', First_Part), nl,
+	call(First_Part),
+	print_msg(1, 3, 'aux', Logo, ' Vars: '),
+	print_vars_diseqs(1, '', Vars), nl,
+	print_msg(1, 3, 'aux', '2nd: ', Second_Part), nl,
+	call(Second_Part),
+	print_msg(1, 3, 'aux', ' Vars: ', ''), nl,
+	print_vars_diseqs(1, 'Vars: ', Vars), nl.
 
-tests_succeed(T1, T2) :- logo('s01'), T1 = a, T2 = a, equality(T1, T2, []).
-tests_succeed(T1, T2) :- logo('s02'), equality(T1, T2, []), equality(T1, T2, []).
-tests_succeed(T1, T2) :- logo('s03'), disequality(T1, T2, []), disequality(T1, T2, []).
-tests_succeed(T1, T2) :- logo('s04'), equality(T1, T2, []), equality(T1, T2, [T1]).
-tests_succeed(T1, T2) :- logo('s05'), equality(T1, s(T3), []), disequality(T1, T2, [T3]).
-tests_succeed(T1, T2) :- logo('s06'), disequality(T1, s(T3), [T3]), disequality(T1, s(T2), [T2]), equality(T1, T2, []).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+tests_fail('f01', [T1 |[ T2]], (T1 = a, T2 = b), equality(T1, T2, [])).
+tests_fail('f02', [T1 |[ T2]], (T1 = a, T2 = a), disequality(T1, T2, [])).
+tests_fail('f03', [T1 |[ T2]], equality(T1, T2, []), disequality(T1, T2, [])).
+tests_fail('f04', [T1 |[ T2]], disequality(T1, T2, []), equality(T1, T2, [])).
+tests_fail('f05', [T1 |[ T2]], equality(T2, s(T1), []), equality(T1, T2, [])).
+tests_fail('f06', [T1 |[ T2]], equality(T1, T2, [T2]), true).
+tests_fail('f07', [T1 |[ T2]], equality(T1, s(T3), []), equality(T1, T2, [T3])).
+tests_fail('f08', [T1 |[ T2]], equality(T1, s(a), []), equality(T1, T2, [T2])).
+tests_fail('f09', [T1 |[ T2]], equality(T1, s(_T3), []), equality(T1, T2, [T2])).
+tests_fail('f10', [T1 |[ T2]], disequality(T1, T2, [T2]), true).
+
+tests_succeed('s01', [T1 |[ T2]], (T1 = a, T2 = a), equality(T1, T2, [])).
+tests_succeed('s02', [T1 |[ T2]], equality(T1, T2, []), equality(T1, T2, [])).
+tests_succeed('s03', [T1 |[ T2]], disequality(T1, T2, []), disequality(T1, T2, [])).
+tests_succeed('s04', [T1 |[ T2]], equality(T1, T2, []), equality(T1, T2, [T1])).
+tests_succeed('s05', [T1 |[ T2]], equality(T1, s(T3), []), disequality(T1, T2, [T3])).
+tests_succeed('s06', [T1 |[ T2]], (disequality(T1, s(T3), [T3]), disequality(T1, s(T2), [T2])), equality(T1, T2, [])).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
