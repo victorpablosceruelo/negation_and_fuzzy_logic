94045826ec58958d1e0d51b974582dc7635b4f74
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-18 18:42:26 +0200

    Trying to make a improved version of the example.

--- negation/cneg/examples/ex_peano_queens.pl
+++ negation/cneg/examples/ex_peano_queens.pl
@@ -52,22 +52,22 @@ test('3 queens -+', (cneg(queens(s(s(s(0))), C))), 'should_succeed', (queens(s(s
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 complex_tests(N, Columns) :- 
-	queens_list(N, List), % This avoids an infinite number of sols problem.
+	queens1_list(N, List), % This avoids an infinite number of sols problem.
 	print_msg(1, 3, 'nl', '', ''),
 	print_msg(1, 3, 'aux', 'tests(N, List) :: (N, List) :: ', (N, List)),
 	print_msg(1, 3, 'nl', '', ''),
 	(
 	    (
-		print_msg(1, 3, '', ' -- STARTED (-) -- ', cneg(queens(N, Columns))), 
-		cneg(queens(N, Columns)), % First constraints.
-		print_msg_with_diseqs(1, 3, ' -- READY (-) -- ', cneg(queens(N, Columns))),
+		print_msg(1, 3, '', ' -- STARTED (-) -- ', cneg(queens1(N, Columns))), 
+		cneg(queens1(N, Columns)), % First constraints.
+		print_msg_with_diseqs(1, 3, ' -- READY (-) -- ', cneg(queens1(N, Columns))),
 		complex_tests_aux_for_negative(N, Columns)
 	    )
 	;
 	    (
-		print_msg(1, 3, '', ' -- STARTED (+) -- ', cneg(queens(N, Columns))), 
-		queens(N, Columns), % First values generator.
-		print_msg(1, 3, '', ' -- READY (+) -- ', cneg(queens(N, Columns))),
+		print_msg(1, 3, '', ' -- STARTED (+) -- ', cneg(queens1(N, Columns))), 
+		queens1(N, Columns), % First values generator.
+		print_msg(1, 3, '', ' -- READY (+) -- ', cneg(queens1(N, Columns))),
 		complex_tests_aux_for_positive(N, Columns)
 	    )
 	).
@@ -75,13 +75,13 @@ complex_tests(N, Columns) :-
 complex_tests_aux_for_negative(N, Columns) :-
 	(
 	    (
-		queens(N, Columns), % Secondly values generator.
-		print_msg_with_diseqs(1, 3, ' -- FAILED (-) -- ', queens(N, Columns))
+		queens1(N, Columns), % Secondly values generator.
+		print_msg_with_diseqs(1, 3, ' -- FAILED (-) -- ', queens1(N, Columns))
 		% !, fail
 	    )
 	;
 	    (
-		print_msg(1, 3, '', ' -- PASSED (-) -- ', queens(N, Columns)),
+		print_msg(1, 3, '', ' -- PASSED (-) -- ', queens1(N, Columns)),
 		!, fail
 	    )
 	).
@@ -89,13 +89,13 @@ complex_tests_aux_for_negative(N, Columns) :-
 complex_tests_aux_for_positive(N, Columns) :-
 	(
 	    (
-		cneg(queens(N, Columns)), % Second constraints.
-		print_msg_with_diseqs(1, 3, ' -- FAILED (+) -- ', queens(N, Columns))
+		cneg(queens1(N, Columns)), % Second constraints.
+		print_msg_with_diseqs(1, 3, ' -- FAILED (+) -- ', queens1(N, Columns))
 		% !, fail
 	    )
 	;
 	    (
-		print_msg(1, 3, '', ' -- PASSED (+) -- ', queens(N, Columns)),
+		print_msg(1, 3, '', ' -- PASSED (+) -- ', queens1(N, Columns)),
 		!, fail
 	    )
 	).
@@ -110,45 +110,45 @@ complex_tests_aux_for_positive(N, Columns) :-
 %        queens(s(s(s(s(0)))),[s(s(0)),s(s(s(s(0)))),s(0),s(s(s(0)))])
 % means that the 4 queens are placed in positions (1,2),(2,4),(3,1) and (4,3). 
 
-queens(N, Columns):- 
-        queens_list(N, Ns),
-        queens1(Ns, [], Columns).     % To be placed, placed, result
+queens1(N, Columns):- 
+        queens1_list(N, Ns),
+        queens1_aux(Ns, [], Columns).     % To be placed, placed, result
 
 % queens_list(N, List) generates a list of N queens in List.
 % The order does not matter yet.
-queens_list(0, []).
-queens_list(s(N), [s(N)|List]) :-
-        queens_list(N, List).
+queens1_list(0, []).
+queens1_list(s(N), [s(N)|List]) :-
+        queens1_list(N, List).
 
 % queens1(Ns,[],Columns) returns in Columns a permutation of the columns represented
 % in Ns such as there will be secure position for placing queens in them.
 % queens1(Unplaced, Placed, Columns) appends to Placed columns the columns of 
 % Unplaced in a secure way for all the queens.
-queens1([], Columns, Columns). 
-queens1([X|Unplaced], Placed, Columns):-
-        select_column(Column, [X|Unplaced], NewUnplaced),
-        no_attack(Placed, Column, Column),
-        queens1(NewUnplaced, [Column|Placed], Columns).
+queens1_aux([], Columns, Columns). 
+queens1_aux([X|Unplaced], Placed, Columns):-
+        queens1_reorder_queens(Column, [X|Unplaced], NewUnplaced),
+        queens1_no_attack(Placed, Column, Column),
+        queens1_aux(NewUnplaced, [Column|Placed], Columns).
  
 % select(X, Ys, Zs) X is an element of Ys and Zs is Ys except X
-select_column(X, [X|Ys], Ys).
-select_column(X, [Y|Ys], [Y|Zs]):-
-        select_column(X, Ys, Zs).
+queens1_reorder_queens(X, [X|Ys], Ys).
+queens1_reorder_queens(X, [Y|Ys], [Y|Zs]):-
+        queens1_reorder_queens(X, Ys, Zs).
 
 % no_attack(Placed, Q, Q) checks that 
 % a queen in the next row to the ones in Placed 
 % doesn't attack any queen there.
-no_attack([], _Col_Add, _Col_Subst).
-no_attack([Y|Ys], Col_Add, 0):-
+queens1_no_attack([], _Col_Add, _Col_Subst).
+queens1_no_attack([Y|Ys], Col_Add, 0):-
 	add(Col_Add, s(0), New_Col_Add),
         disequality(New_Col_Add, Y, []),
-        no_attack(Ys, New_Col_Add, 0).
-no_attack([Y|Ys], Col_Add, Col_Subst):-
+        queens1_no_attack(Ys, New_Col_Add, 0).
+queens1_no_attack([Y|Ys], Col_Add, Col_Subst):-
 	add(Col_Add, s(0), New_Col_Add),
         disequality(New_Col_Add, Y, []),
 	subst(Col_Subst, s(0), New_Col_Subst),
 	disequality(New_Col_Subst, Y, []),
-        no_attack(Ys, New_Col_Add, New_Col_Subst).
+        queens1_no_attack(Ys, New_Col_Add, New_Col_Subst).
 
 add(0,X,X). 
 add(s(X),Y,s(Z)) :-
