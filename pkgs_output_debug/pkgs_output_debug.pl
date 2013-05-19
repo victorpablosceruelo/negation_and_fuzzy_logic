68958f81981550d4b32194b3ef1f1b483c307ec2
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-19 19:20:25 +0200

    Renaming examples.

--- pkgs_output_debug/pkgs_output_debug.pl
+++ pkgs_output_debug/pkgs_output_debug.pl
@@ -2,7 +2,7 @@
 :- load_compilation_module(library('pkgs_output_debug/pkgs_output_debug_tr')).
 % To use it when it is not a system library.
 % :- load_compilation_module(.(pkgs_output_debug_tr)).
-:- add_sentence_trans(pkgs_output_debug/3, 740).
+:- add_sentence_trans(pkgs_output_debug_tr:pkgs_output_debug/3, 740).
 % :- add_clause_trans(debugger_pkg/3).
 % :- translation_predname(debugger_pkg/3).
 
