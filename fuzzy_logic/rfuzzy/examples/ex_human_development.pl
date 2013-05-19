cce15a4e635586de69d6dd296f0481bae2a03d54
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-19 20:12:57 +0200

    Fixing examples.

--- fuzzy_logic/rfuzzy/examples/ex_human_development.pl
+++ fuzzy_logic/rfuzzy/examples/ex_human_development.pl
@@ -1,46 +1,24 @@
-:- module(human_development,_,[rfuzzy, clpr]).
+:- module(ex_human_development,_,[rfuzzy, clpr]).
 
-type_Country(japan).
-type_Country(spain).
-type_Country(china).
-type_Country(australia).
-type_Country(cuba).
-type_Country(brasil).
-type_Country(egypt).
-type_Country(kenya).
-type_Country(senegal).
+rfuzzy_define_database(country/4, 
+	[(id, rfuzzy_string_type), 
+	  (living_standard, rfuzzy_truth_value_type), 
+	   (literacy_rate, rfuzzy_truth_value_type),
+	    (long_life, rfuzzy_truth_value_type)]).
 
-rfuzzy_type_for(living_standard/1, [type_Country/1]).
-rfuzzy_type_for(literacy_rate/1, [type_Country/1]).
-rfuzzy_type_for(long_life/1, [type_Country/1]).
-rfuzzy_type_for(human_development/1, [type_Country/1]).
+country(japan,      0.95,  null,  0.82).
+country(spain,      0.9,    0.97, 0.79).
+country(china,      0.65,  0.9,   null).
+country(australia, 0.95,  null,  0.8).
+country(cuba,       null,   0.96, 0.77).
+country(brasil,      0.63,  0.88, null).
+country(egypt,      null,   0.55, null). 
+country(kenya,     0.13,  0.73, 0.48).
+country(senegal,  0.25,  0.39, 0.59).
 
-rfuzzy_default_value_for(living_standard/1, 0.48).
-rfuzzy_default_value_for(literacy_rate/1, 0.99).
-rfuzzy_default_value_for(long_life/1, 0.72).
+living_standard(country) :~ defaults_to(0.48).
+literacy_rate(country) :~ defaults_to(0.99).
+long_life(country) :~ defaults_to(0.72).
 
-living_standard(japan) value 0.95.
-living_standard(spain) value 0.9.
-living_standard(china) value 0.65.
-living_standard(australia) value 0.95.
-living_standard(brasil) value 0.63.
-living_standard(kenya) value 0.13.
-living_standard(senegal) value 0.25.
-
-literacy_rate(spain) value 0.97.
-literacy_rate(china) value 0.9.
-literacy_rate(cuba) value 0.96.
-literacy_rate(brasil) value 0.88.
-literacy_rate(egypt) value 0.55.
-literacy_rate(kenya) value 0.73.
-literacy_rate(senegal) value 0.39.
-
-long_life(japan) value 0.82.
-long_life(spain) value 0.79.
-long_life(australia) value 0.8.
-long_life(cuba) value 0.77.
-long_life(kenya) value 0.48.
-long_life(senegal) value 0.59.
-
-human_development(C) :~ prod((literacy_rate(C),long_life(C),living_standard(C))).
+human_development(country) :~ rule(prod, (literacy_rate(country),long_life(country),living_standard(country))).
 
