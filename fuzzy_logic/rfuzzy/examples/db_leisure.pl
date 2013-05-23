7ed8b2302112f1d3791c6dbf5b689838d932a61c
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-23 21:59:57 +0200

    remove debug output.

--- fuzzy_logic/rfuzzy/examples/db_leisure.pl
+++ fuzzy_logic/rfuzzy/examples/db_leisure.pl
@@ -1,4 +1,4 @@
-:- module(db_leisure,_,[rfuzzy, pkgs_output_debug, clpr]).
+:- module(db_leisure,_,[rfuzzy, clpr]).
 % Compilation time debug can be activated  by adding to the packages list [rfuzzy, clpr] the package pkgs_output_debug.
 % Running time debug can be activated removing the comment marker % at the beginning of the following line.
 % :- activate_rfuzzy_debug.
