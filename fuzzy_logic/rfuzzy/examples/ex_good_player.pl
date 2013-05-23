3be51ac983876505d6f7aa7c121976edcb72de3e
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-05-23 21:52:26 +0200

    Fixed example.

--- fuzzy_logic/rfuzzy/examples/ex_good_player.pl
+++ fuzzy_logic/rfuzzy/examples/ex_good_player.pl
@@ -1,59 +1,43 @@
-:- module(good_player,_,[rfuzzy, clpr]).
+:- module(ex_good_player,_,[rfuzzy, clpr]).
+
+% Define the database.
+rfuzzy_define_database(player/4, 
+	[(id, rfuzzy_enum_type), 
+	  (swift, rfuzzy_truth_value_type),
+	   (height, rfuzzy_integer_type), % Height of players in mm.
+	    (years_playing, rfuzzy_integer_type)]).
 
 % Define the individuals belonging to the set player.
-player(john).
-player(karl).
-player(mike).
-player(lebron).
-player(deron).
-player(damian).
-player(aito).
+player(john, 1, 1700, 2).
+player(karl, 0.6, 1900, 3).
+player(mike, 0.9, null, null).
+player(lebron, 1, 1950, null).
+player(deron, 0.8, 2000, 7).
+player(damian, null, 1850, 1).
+player(aito, null, 1800, null).
+player(marcus, null, 1780, null).
 
 
-rfuzzy_type_for(swift/1, [player/1]).
-rfuzzy_default_value_for(swift/1,0.5).
+% If no info provided a player is considered to be swift with a truth value of 0.5.
+swift(player) :~ defaults_to(0.5).
 
-swift(john) value 1.
-swift(karl) value 0.6 .
-swift(mike) value 0.9 .
-swift(lebron) value 1.
-swift(deron) value 0.8.
+tall(player) :~ defaults_to(0).
+tall(player) :~ function(height(player), [ (1500, 0), (1800, 0.5), (2000, 1), (3000, 1) ]).
 
-% Height of players in mm.
-height(john, 1700).
-height(marcus, 1780).
-height(aito, 1800).
-height(damian, 1850).
-height(karl, 1900).
-height(lebron, 1950).
-height(deron, 2000).
-
-% Tall predicate depending on the player's height.
-% OLD: tall(Player, V) :- height(Player, H), tall_func(H, V).
-tall_function :# ([ (1500, 0), (1800, 0.5), (2000, 1), (3000, 1) ]).
-rfuzzy_define_fuzzification(tall/1, height/2, tall_function/2).
-
-rfuzzy_type_for(experience/1, [player/1]).
-rfuzzy_default_value_for(experience/1, 0.1).
-
-experience(lebron) value 0.4.
-experience(deron) value 0.3.
+experience(player) :~ defaults_to(0.1).
+experience(player) :~ function(years_playing(player), [(0,0), (2, 0.2), (5, 1)]).
 
 
-% Define that only player are valid individuals for
-% the fuzzy set good_player.
-rfuzzy_type_for(good_player/1, [player/1]).
-
-% An individual is a good player with a truth value of 0.1
+% A player is a good player with a truth value of 0.1
 % if we can not compute a more accurate value.
-rfuzzy_default_value_for(good_player/1, 0.1).
+good_player(player) :~ defaults_to(0.1).
 
 % The rule to determine the grade of belonging of 
 % a player to the fuzzy set of good_player has a 
 % confidence of 0.8. Its result is a combination of 
 % how much (the truth value) he/she is swift, tall and 
 % an experienced player. 
-good_player(J) cred (prod,0.8) :~ prod((swift(J), tall(J), experience(J))).
+good_player(player)  :~ rule(prod, (swift(player), tall(player), experience(player))) with_credibility (prod,0.8). 
 
 % Queries (examples).
 
