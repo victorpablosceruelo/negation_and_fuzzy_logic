c9dbb25c7667eebab4e933705aa60b17212937ef
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-09-03 21:08:34 +0200

    Fixed reflection problems.

--- fuzzy_logic/flese/src/managers/AbstractManager.java
+++ fuzzy_logic/flese/src/managers/AbstractManager.java
@@ -1,7 +1,11 @@
 package managers;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import storeHouse.RequestStoreHouse;
 import urls.UrlMap;
+import urls.UrlMapException;
 import urls.UrlsMaps;
 import auxiliar.NextStep;
 import constants.KConstants;
@@ -22,25 +26,64 @@ public abstract class AbstractManager implements InterfaceManager {
 		return requestStoreHouse.getRequestParameter(KConstants.Request.operationParam);
 	}
 
-	public NextStep processRequest() throws Exception {
+	public NextStep processRequest() {
 
 		NextStep nextStep = null;
+		String op = null;
 
 		UrlMap urlMap = new UrlMap(requestStoreHouse);
-		urlMap = UrlsMaps.getUrlMap(urlMap);
+		try {
+			urlMap = UrlsMaps.getUrlMap(urlMap);
+		} catch (UrlMapException e1) {
+			e1.printStackTrace();
+			urlMap = null;
+		}
 
-		String op = urlMap.getOp();
-		if ((op != null) && (!"".equals(op))) {
-			try {
-			nextStep = (NextStep) this.getClass().getMethod(op, (Class<?>) null).invoke((Object) null, (Object) null);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-				op = null;
+		if (urlMap != null) {
+			op = urlMap.getOp();
+			if ((op != null) && (!"".equals(op))) {
+
+				@SuppressWarnings("rawtypes")
+				Class[] argTypes = new Class[] {};
+				Method method = null;
+				try {
+					method = this.getClass().getMethod(op, argTypes);
+				} catch (NoSuchMethodException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SecurityException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (method != null) {
+					try {
+						nextStep = (NextStep) method.invoke((Object) this, new Object [0]);
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InvocationTargetException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (Exception e) {
+						e.printStackTrace();
+						// throw e;
+					}
+				}
+
 			}
-		} 
-		// This allows a failsafe when the method is not found or there is no method.
-		if ((op == null) || ("".equals(op))) {
-			nextStep = byDefaultMethod();
+		}
+		// This allows a failsafe when the method is not found or there is no
+		// method.
+		if ((urlMap == null) || (op == null) || ("".equals(op))) {
+			try {
+				nextStep = byDefaultMethod();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return nextStep;
 	}
