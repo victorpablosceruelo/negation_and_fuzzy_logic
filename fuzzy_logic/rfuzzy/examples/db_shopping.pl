716ecc98e877309dce8610917bbd7fe4f7c6a3c2
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-11-06 20:15:03 +0100

    Fixed cars db.

--- fuzzy_logic/rfuzzy/examples/db_shopping.pl
+++ fuzzy_logic/rfuzzy/examples/db_shopping.pl
@@ -1,9 +1,6 @@
-:- module(db_shopping, _, [rfuzzy, clpr]).
-% Compilation time debug can be activated  by adding to the packages list [rfuzzy, clpr] the package pkgs_output_debug.
-% Running time debug can be activated removing the comment marker % at the beginning of the following line.
-% :- activate_rfuzzy_debug.
+:- module(db_shopping, _, [rfuzzy, clpr, pkgs_output_debug]).
 
-% TYPE DECLARATION
+% Database declaration
 rfuzzy_define_database(car/11, 
 	[(id, rfuzzy_enum_type), 
 	  (manufacturer, rfuzzy_enum_type),
@@ -17,17 +14,18 @@ rfuzzy_define_database(car/11,
 		  (max_speed_in_mph, rfuzzy_integer_type),
 		   (price_in_euros, rfuzzy_integer_type)]).
 
-% We define the individuals belonging to th set car.
-car('Fiat Cinquecento', 'Fiat Auto Poland', 'City Car', 1991, 1998, 3230, 1490, 1435, 31, 93, null).
-car('VW Caddy', 'Volkswagen Commercial Vehicles', 'Work Van', 1980, null, 1794, 4405, 1833, 80, 110, 45000).
-car('Alfa Romeo GT', 'Alfa Romeo', 'Sport Car', 2003, 2010, 4489, 1763, 1362, 237, 130, 30000).
-car('Aston Martin Bulldog', 'Aston Martin', 'Sport Car', 1980, null, 4376, 1698, 1100, 522, 191, 150000).
-car('Ford Fiesta', 'Ford Motor Company', 'City Car', 1976, null, 3924, 1685, 1468, 73, 114, 12000).
+% We define the individuals in our database.
+car('Fiat Cinquecento', 'Fiat Auto Poland', 'City Car',                     1991, 1998, 3230, 1490, 1435, 31, 93, null).
+car('VW Caddy', 'Volkswagen Commercial Vehicles', 'Work Van', 1980, null,   4405, 1794, 1833, 80, 110, 45000).
+car('Alfa Romeo GT', 'Alfa Romeo', 'Sport Car',                            2003, 2010, 4489, 1763, 1362, 237, 130, 30000).
+car('Aston Martin Bulldog', 'Aston Martin', 'Sport Car',                  1980, null,    4376, 1698, 1100, 522, 191, 150000).
+car('Ford Fiesta', 'Ford Motor Company', 'City Car',                      1976, null,    3924, 1685, 1468, 73, 114, 12000).
+
 %  vw_phaeton.
 % ferrari
 
 expensive(car) :~ function(price_in_euros(car), [ (0, 0), (10000, 0.5), (30000, 1), (1000000, 1) ]).
-% Future syntax
-% expensive(car) :~ function(price_in_euros(car), [(0, 0), (10000, 0.5), (30000, 1)]).
-
 fast(car) :~ function(max_speed_in_mph(car), [ (0, 0), (100, 0.5), (150, 1), (1000, 1) ]).
+large(car) :~ function(length(car), [ (0, 0), (100, 0.5), (150, 1), (1000, 1) ]).
+wide(car) :~ function(width(car), [ (0, 0), (100, 0.5), (150, 1), (1000, 1) ]).
+big(car) :~ rule(min, (large(car), wide(car))).
