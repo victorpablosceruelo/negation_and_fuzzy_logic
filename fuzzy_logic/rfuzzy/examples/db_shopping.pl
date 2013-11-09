96bd1160df80cf3d5c6106f93cd0c6a77c4306c0
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2013-11-09 14:41:27 +0100

    improving the example.

--- fuzzy_logic/rfuzzy/examples/db_shopping.pl
+++ fuzzy_logic/rfuzzy/examples/db_shopping.pl
@@ -1,7 +1,7 @@
 :- module(db_shopping, _, [rfuzzy, clpr, pkgs_output_debug]).
 
 % Database declaration
-rfuzzy_define_database(car/11, 
+rfuzzy_define_database(car/17, 
 	[(id, rfuzzy_enum_type), 
 	  (manufacturer, rfuzzy_enum_type),
 	   (class,  rfuzzy_enum_type),
@@ -11,32 +11,35 @@ rfuzzy_define_database(car/11,
 	       (car_width_in_mm, rfuzzy_integer_type),
 		(car_height_in_mm, rfuzzy_integer_type),
 		 (horsepower_in_kw, rfuzzy_integer_type),
-		  (acceleration_0_to_100_kph_in_s, rfuzzy_integer_type),
+		  (acceleration_to_100kph_in_s, rfuzzy_integer_type),
 		   (max_speed_in_kph, rfuzzy_integer_type),
 		    (number_of_cylinders, rfuzzy_integer_type),
 		     (number_of_valves, rfuzzy_integer_type),
 		      (fuel_tank_capacity_in_litres, rfuzzy_integer_type),
-		       (consumption_in_litres_each_100_km, rfuzzy_integer_type),
-			(price_in_euros, rfuzzy_integer_type)]).
+		       (fuel_type, rfuzzy_enum_type),
+			(consumption_in_litres_each_100_km, rfuzzy_integer_type),
+			 (price_in_euros, rfuzzy_integer_type)]).
 
 % We define the individuals in our database.
-car('Fiat Cinquecento', 'Fiat Auto Poland', 'City Car',                     1991, 1998,  3230, 1490, 1435,    40,    18,  140, 4,  null, 35, 5.9,         null).
-car('VW Caddy', 'Volkswagen Commercial Vehicles', 'Work Van', 1980, null,    4405, 1794, 1833,    80, 10.3, 110, 4,   null, 60, 46.3,   45000).
-car('Alfa Romeo GT', 'Alfa Romeo', 'Sports Car',                            2003, 2010, 4489, 1763, 1362,  237, 10.6, 130, 6,   null, 63, 35,      30000).
-car('Aston Martin Bulldog', 'Aston Martin', 'Sports Car',                  1980, null,    4376, 1698, 1100,  522, 12,    191, 8,   null, 113, 30,    150000).
+car('Fiat Cinquecento', 'Fiat Auto Poland', 'City Car',                     1991, 1998,  3230, 1490, 1435,    40,    18,  140, 4,  null, 35,   g, 5.9,         null).
+car('VW Caddy', 'Volkswagen Commercial Vehicles', 'Work Van', 1980, null,    4405, 1794, 1833,    80, 10.3, 110, 4,   null, 60,   g, 46.3,   45000).
+car('Alfa Romeo GT', 'Alfa Romeo', 'Sports Car',                            2003, 2010, 4489, 1763, 1362,  237, 10.6, 130, 6,   null, 63,   g, 35,      30000).
+car('Aston Martin Bulldog', 'Aston Martin', 'Sports Car',                  1980, null,    4376, 1698, 1100,  522, 12,    191, 8,   null, 113, g, 30,    150000).
 
-car('Ford Fiesta', 'Ford Motor Company', 'City Car',                      2008, null,      3950, 1722, 1481,   96, 13.9, 166, 4,   16,     45, 6.5,    12000).
-car('Ford Probe', 'Ford Motor Company', 'Sports Coupe',               1989, 1997,  4496, 1735, 1316,  165,   8,   220, 6,   24,     60, 9.6,   20000).
-car('Peugeot 207CC', 'Peugeot', 'Supermini',                                2006, 2012,    4045, 1748, 1472,  156,  8.5, 220, 4,   16,     50, 7.4,    21000).
+car('Ford Fiesta', 'Ford Motor Company', 'City Car',                      2008, null,      3950, 1722, 1481,   96, 13.9, 166, 4,   16,     45, d, 6.5,    12000).
+car('Ford Probe', 'Ford Motor Company', 'Sports Coupe',               1989, 1997,  4496, 1735, 1316,  165,   8,   220, 6,   24,     60, g, 9.6,   20000).
+car('Peugeot 207CC', 'Peugeot', 'Supermini',                                2006, 2012,    4045, 1748, 1472,  156,  8.5, 220, 4,   16,     50, g, 7.4,    21000).
 
-car('Audi TT', 'Audi AG', 'Sports Car',                                             1998, null,     4178, 1735, 1352,  200,  6.6, 240, 4,   16,     55, 7.7,    40000).
-car('Audi TT Quattro Sport', 'Audi AG', 'Sports Car',                      2006, null,     4178, 1842, 1352,  250,  5.7, 250, 6,   24,     60, 9.4,    48000).
+car('Audi TT', 'Audi AG', 'Sports Car',                                             1998, null,     4178, 1735, 1352,  200,  6.6, 240, 4,   16,     55, g, 7.7,    40000).
+car('Audi TT Quattro Sport', 'Audi AG', 'Sports Car',                      2006, null,     4178, 1842, 1352,  250,  5.7, 250, 6,   24,     60, g, 9.4,    48000).
+
+
 
 %  vw_phaeton.
 % ferrari
 
 expensive(car) :~ function(price_in_euros(car), [ (0, 0), (10000, 0.5), (30000, 1), (1000000, 1) ]).
-fast(car) :~ function(max_speed_in_mph(car), [ (0, 0), (60, 0), (100, 0.4), (150, 0.75), (200, 1), (1000, 1) ]).
+fast(car) :~ function(max_speed_in_kph(car), [ (0, 0), (60, 0), (100, 0.4), (150, 0.75), (200, 1), (1000, 1) ]).
 large(car) :~ function(car_length_in_mm(car), [ (0, 0), (3000, 0.2), (4000, 0.5), (5000, 1), (10000, 1) ]).
 wide(car) :~ function(car_width_in_mm(car), [ (0, 0), (1400, 0.2), (1500, 0.5), (1700, 1), (3000, 1) ]).
 big(car) :~ rule(min, (large(car), wide(car))).
