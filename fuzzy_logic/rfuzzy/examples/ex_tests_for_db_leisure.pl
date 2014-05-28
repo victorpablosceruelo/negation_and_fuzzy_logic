242a668fcc0cfad79f1b8c02c392f1323075b72a
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2014-05-28 20:24:29 +0200

    Tests to understand the new syntax.

--- fuzzy_logic/rfuzzy/examples/ex_tests_for_db_leisure.pl
+++ fuzzy_logic/rfuzzy/examples/ex_tests_for_db_leisure.pl
@@ -28,3 +28,9 @@ t3_aux(R, TV1, TV2) :-
 		 true
 	     )
 	 ).
+
+
+t4(A, B, C, D, E, F, G, H, I) :- restaurant(_12, _13, _14, _15, _16, _17, _18), =(_19, restaurant(_12, _13, _14, _15, _16, _17, _18)), rfuzzy_compute(=~=, tapas, food_type(_19), restaurant, _20), rfuzzy_var_truth_value(_19, _22, I), rfuzzy_var_truth_value(_12, _24, H), rfuzzy_var_truth_value(_13, _26, G), rfuzzy_var_truth_value(_14, _28, F), rfuzzy_var_truth_value(_15, _30, E), rfuzzy_var_truth_value(_16, _32, D), rfuzzy_var_truth_value(_17, _34, C), rfuzzy_var_truth_value(_18, _36, B), rfuzzy_var_truth_value(_20, _38, A).
+
+t5(A, B, C, D, E, F, G, H, I) :- restaurant(_12, _13, _14, _15, _16, _17, _18), =(_19, restaurant(_12, _13, _14, _15, _16, _17, _18)), rfuzzy_compute(=~=, spanish, food_type(_19), restaurant, _20), rfuzzy_var_truth_value(_19, _22, I), rfuzzy_var_truth_value(_12, _24, H), rfuzzy_var_truth_value(_13, _26, G), rfuzzy_var_truth_value(_14, _28, F), rfuzzy_var_truth_value(_15, _30, E), rfuzzy_var_truth_value(_16, _32, D), rfuzzy_var_truth_value(_17, _34, C), rfuzzy_var_truth_value(_18, _36, B), rfuzzy_var_truth_value(_20, _38, A).
+
