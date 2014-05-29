436374df7536db60f6a9bd2a17ed08f6fbc986dc
Victor Pablos Ceruelo <vpablos@babel.ls.fi.upm.es>
2014-05-29 21:43:51 +0200

    modifiers ...

--- fuzzy_logic/rfuzzy/examples/ex_modifier.pl
+++ fuzzy_logic/rfuzzy/examples/ex_modifier.pl
@@ -1,4 +1,4 @@
-:- module(ex_quantifier,_,[rfuzzy, clpr, pkgs_output_debug]).
+:- module(ex_modifier,_,[rfuzzy, clpr, pkgs_output_debug]).
 % Compilation time debug can be activated  by adding to the packages list [rfuzzy, clpr] the package pkgs_output_debug.
 % Running time debug can be activated removing the comment marker % at the beginning of the following line.
 % :- activate_rfuzzy_debug.
